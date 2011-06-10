0d69bbd9c50b21976c6826b8e9057f560175d325
Andrew Cates <catesandrew@netflix.com>
2011-06-09 21:18:10 -0700

    added the bash customizations

---
+++ home/_bash/aliases.sh
@@ -0,0 +1,54 @@
+#######################################################################
+# ~/.bash/aliases.sh                                                  #
+# version 0.2.1                                                       #
+# by Paul Duncan <pabs@pablotron.org>                                 #
+#######################################################################
+
+########################
+# Basic System Aliases #
+########################
+
+#alias ls="BLOCK_SIZE=\'1 ls --color=auto" #enable thousands grouping and colour
+#alias units='units -t' #terse mode
+#alias diff='LC_ALL=C TZ=GMT0 diff -Naur' #normalise diffs for distribution #normalise diffs for distribution
+#alias xterm='xterm -fb "" -bg black -fg gray -fa "Sans Mono" -fs 9 +sb -sl 3000 -g 80x50+1+1'
+#alias sudo='sudo env PATH=$PATH' #work around sudo built --with-secure-path (ubuntu)
+#alias vim='vim -X' #don't try to contact xserver (which can hang on network issues)
+#alias gdb='gdb -tui -quiet' #enable the text window interface if possible
+#alias head='head -n $((${LINES:-12}-2))' #as many as possible without scrolling
+#alias tail='tail -n $((${LINES:-12}-2)) -s.1' #Likewise, also more responsive -f
+
+alias ll='ls -l'
+alias la='ls -A'
+alias dir='ls --color=auto --format=vertical'
+alias vdir='ls --color=auto --format=long'
+
+# enable directory colors
+#eval `dircolors`
+#alias ls='ls --color=auto '
+
+# misc
+alias ..="cd .."        #go to parent dir
+alias ...="cd ../.."    #go to grandparent dir
+alias -- -="cd -"       #go to previous dir
+alias l.='ls -d .*'     #list hidden files
+alias ll='ls -lhrt'     #extra info compared to "l"
+alias lld='ls -lUd */'  #list directories
+alias cls="clear"
+
+# GREP_COLOR=bright yellow on black bg.
+# use GREP_COLOR=7 to highlight whitespace on black terminals
+# LANG=C for speed. See also: http://www.pixelbeat.org/scripts/findrepo
+#alias grep='GREP_COLOR="1;33;40" LANG=C grep --color=auto'
+
+# I hate noise
+set bell-style visible
+
+# Tell less not to beep and also display colours
+#export LESS="-QR"
+# Adjust the less highlight colors
+#export LESS_TERMCAP_so="$(printf 'rev\nbold\nsetaf 3\n' | tput -S)"
+#export LESS_TERMCAP_se="$(tput sgr0)"
+
+# Let me have core dumps
+#ulimit -c unlimited
---
+++ home/_bash/functions.sh
@@ -0,0 +1,81 @@
+#######################################################################
+# ~/.bash/functions.sh                                                #
+# version 0.2.1                                                       #
+# by Paul Duncan <pabs@pablotron.org>                                 #
+#######################################################################
+
+# moved this to a shell script
+# you can get it from http://www.pablotron.org/downloads/google
+#function google() { TF=/tmp/wget-"$USER"-"$RANDOM".html; wget -O $TF -q http://www.google.com/search?q=`echo $1|sed 's/ /+/g'`; perl -nle 'if (/href=/i) { $t=""; $u=""; /<A HREF=(.*?)>(.*?)<\/A>/; $t=$2; $u=$1; if ($t && $u && $t !~ /^(<IMG|Cached)/ && $u !~ /^\//) { $c++; $t =~ s/<\/?b>//gi; print "$c. \"$t\":\n    $u"; } }' < $TF; rm $TF; }
+
+function growl() { echo -e $'\e]9;'${1}'\007' ; return  ; }
+
+# cdf: cd to the directory in the Finder's front window
+alias cdf='cd "$(/usr/local/bin/posd)"'
+
+# posfind: search the directory frontmost in the Finder
+function posfind { find "`/usr/local/bin/posd`" -name "*$1*"; }
+
+# posgrep: grep the directory frontmost in the Finder
+function posgrep { grep -iIrn "$1" "`/usr/local/bin/posd`"; }
+
+function ecweb {
+  nf status;nf kill;nf status;nf run ecweb;nf status;nf log ecweb
+}
+
+function tom {
+	if [ "`ps xwww | grep -v grep | grep -c catalina`" == "0" ];then
+		echo "Off";
+	else
+		CATALINA_PID=`ps A | grep -v grep | grep catalina | awk '{ print $1 }' | sed 's/[ \t]*$//'`;
+		echo "On - $CATALINA_PID";
+	fi
+}
+
+function killtom {
+	if [ "`ps xwww | grep -v grep | grep -c catalina`" == "1" ]; then
+		CATALINA_PID=`ps A | grep -v grep | grep catalina | awk '{ print $1 }' | sed 's/[ \t]*$//'`;
+	  kill -9 $CATALINA_PID
+		echo "Tom is dead. Killed process $CATALINA_PID"
+	else
+		echo "Tom is not on."
+	fi	
+}
+
+export LG_BROWSER LG_PREFIX
+
+function brainyquote {
+  curl --silent http://www.brainyquote.com | egrep '(span class="body")|(span class="bodybold")' | sed -n '6p; 7p; ' | sed ' s/<[a-z0-9=."/ ]*>//g'
+}
+
+function quoteoftheday {
+  echo `curl --silent http://www.quotedb.com/quote/quote.php?action=quote_of_the_day_rss | awk 'NR==23' | sed -e 's/<[^>]*>//g'`
+  echo `curl --silent http://www.quotedb.com/quote/quote.php?action=quote_of_the_day_rss | awk 'NR==22' | sed -e 's/<[^>]*>//g'`
+}
+
+
+##################################################
+# Fancy PWD display function
+##################################################
+# The home directory (HOME) is replaced with a ~
+# The last pwdmaxlen characters of the PWD are displayed
+# Leading partial directory names are striped off
+# /home/me/stuff          -> ~/stuff               if USER=me
+# /usr/share/big_dir_name -> ../share/big_dir_name if pwdmaxlen=20
+# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x783.html
+##################################################
+bash_prompt_command() {
+    # How many characters of the $PWD should be kept
+    local pwdmaxlen=25
+    # Indicate that there has been dir truncation
+    local trunc_symbol=".."
+    local dir=${PWD##*/}
+    pwdmaxlen=$(( ( pwdmaxlen < ${#dir} ) ? ${#dir} : pwdmaxlen ))
+    NEW_PWD=${PWD/#$HOME/\~}
+    local pwdoffset=$(( ${#NEW_PWD} - pwdmaxlen ))
+    if [ ${pwdoffset} -gt "0" ]
+    then
+        NEW_PWD=${NEW_PWD:$pwdoffset:$pwdmaxlen}
+        NEW_PWD=${trunc_symbol}/${NEW_PWD#*/}
+    fi
+}
