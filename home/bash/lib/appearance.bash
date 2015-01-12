49d9e4d4afc1d3623775300b756816ee374786e1
Andrew Cates <catesandrew@gmail.com>
2015-01-12 10:45:41 -0800

    Remove prompt command

--- home/bash/lib/appearance.bash
+++ home/bash/lib/appearance.bash
@@ -12,3 +12,9 @@ export LSCOLORS='Gxfxcxdxdxegedabagacad'
 if [[ $BASH_IT_THEME ]]; then
     source "$BASH_IT/themes/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
 fi
+
+if [[ $PROMPT_COMMANI ]]; then
+    # Make new shells get the history lines from all previous
+    # shells instead of the default "last window closed" history
+    PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+fi
