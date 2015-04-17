bc3fdc1ab6d5ab359035df88384f471d964d8987
Andrew Cates <catesandrew@gmail.com>
2015-04-17 05:01:56 -0700

    Remove v alias -> was interferring with fasd

--- home/bash/aliases/vim.aliases.bash
+++ home/bash/aliases/vim.aliases.bash
@@ -1,8 +1,6 @@
 cite 'about-alias'
 about-alias 'vim abbreviations'
 
-alias v='mvim --remote-tab'
-
 function gvim() {
     local DIR_NAME=`git rev-parse --show-toplevel`
     echo "Running mvim --servername `basename ${DIR_NAME}`"
