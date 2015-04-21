434f1d0c1dd4b7cfc302200ae61ddbd81e9ff490
Andrew Cates <catesandrew@gmail.com>
2015-04-21 12:46:20 -0700

    Update emacs aliases

--- home/bash/aliases/emacs.aliases.bash
+++ home/bash/aliases/emacs.aliases.bash
@@ -8,10 +8,9 @@ case $OSTYPE in
     alias E='SUDO_EDITOR="emacsclient" sudo -e'
     ;;
   darwin*)
-    # alias em='open -a emacs'
     alias em=__emacs_free
     alias ec=__emacs_client
-    alias es=__emacs_stuck
+    alias memacs=__emacs_stuck
     ;;
 esac
 
@@ -28,14 +27,13 @@ function __emacs_free
 
 function __emacs_stuck
 {
-    ${BREW_HOME}/homebrew/Cellar/emacs-mac/HEAD/Emacs.app/Contents/MacOS/Emacs "$@"
-
+    # $(brew --prefix emacs-mac)/Emacs.app/Contents/MacOS/Emacs "$@"
+    ${BREW_EMACS_MAC_HOME}/Emacs.app/Contents/MacOS/Emacs "$@" & >/dev/null
 }
 
 function __emacs_client
 {
     osascript -e 'tell application "Emacs" to activate'
-    # /usr/local/bin/emacsclient -c -n -a "" "$@"
     exec /usr/bin/env emacsclient -c -n -a "" $*
 }
 
