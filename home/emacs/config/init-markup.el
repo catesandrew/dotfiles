754e653c56f0b06c2b1b23af6f9d103b37b83c7a
Andrew Cates <catesandrew@gmail.com>
2015-04-26 06:53:29 -0700

    Add markdown settings, increase clojure settings

--- home/emacs/config/init-markup.el
+++ home/emacs/config/init-markup.el
@@ -7,5 +7,23 @@
 (add-hook 'toml-mode-hook (lambda () (electric-indent-local-mode -1)))
 (add-hook 'markdown-mode-hook (lambda () (electric-indent-local-mode -1)))
 
+;; markdown
+(setq markdown-imenu-generic-expression
+      '(("title"  "^\\(.*\\)[\n]=+$" 1)
+        ("h2-"    "^\\(.*\\)[\n]-+$" 1)
+        ("h1"   "^# \\(.*\\)$" 1)
+        ("h2"   "^## \\(.*\\)$" 1)
+        ("h3"   "^### \\(.*\\)$" 1)
+        ("h4"   "^#### \\(.*\\)$" 1)
+        ("h5"   "^##### \\(.*\\)$" 1)
+        ("h6"   "^###### \\(.*\\)$" 1)
+        ("fn"   "^\\[\\^\\(.*\\)\\]" 1)
+        ))
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (setq imenu-generic-expression markdown-imenu-generic-expression)))
+
+
 (provide 'init-markup)
 
