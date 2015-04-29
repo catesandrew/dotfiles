97153381c8176339a68dfdc61bf8dd10f0e06863
Andrew Cates <catesandrew@gmail.com>
2015-04-29 05:36:59 -0700

    Add fzf bash

---
+++ home/.fzf.bash
@@ -0,0 +1,8 @@
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source "/usr/local/homebrew/Cellar/fzf/0.9.11/shell/completion.bash"
+
+# Key bindings
+# ------------
+source "/usr/local/homebrew/Cellar/fzf/0.9.11/shell/key-bindings.bash"
+
