9d5b94e30742984477ad8af03eee1658ca38e232
Andrew Cates <catesandrew@gmail.com>
2015-05-21 21:51:16 -0700

    Ubuntu aliases for ubuntu only

--- home/.bash/aliases/ubuntu.aliases.bash
+++ home/.bash/aliases/ubuntu.aliases.bash
@@ -1,4 +1,4 @@
-if [ $(uname) = "Darwin" ]; then
+if [ $(uname) = "Linux" ]; then
     # https://mkaz.com/2013/01/13/ubuntu-guide-for-mac-converts/
     alias pbcopy=&#039;xclip -selection clipboard&#039;
     alias pbpaste=&#039;xclip -selection clipboard -o&#039;
