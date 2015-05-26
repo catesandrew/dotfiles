420eac73fb10d23da40eb603bcd6b3017708f7b3
Andrew Cates <catesandrew@gmail.com>
2015-05-26 11:34:35 -0700

    Make dynamic ip dns interchangable
    Works with at least openDNS, google, and changeip as of now.

--- home/.bin/rinker.sh
+++ home/.bin/rinker.sh
@@ -33,58 +33,80 @@
 #################################################################
 
 ################ Script Variables ###############################
-IPPATH=${HOME}/Library/Logs/IP                    # IP address storage file
-TMPIP=/tmp/tmpIP                      # Temp IP storage file
-LOGPATH=${HOME}/Library/Logs/changeip.log         # Log file
-TEMP=/tmp/temp                        # Temp storage file
-USER=${CIPUSER:-"no-user"}
-PASS=${CIPPASS:-"no-pass"}
-SET=${CIPSET:-"1"}
+LOGPATH="${TMPDIR}rinker.log"         # Log file
+
+TMPIP=`mktemp -q "${TMPDIR}"rinker-ip.XXXXXX`   # Temp IP storage file
+if [ $? -ne 0 ]; then
+  echo "$0: Can't create temp IP address storage file, exiting..."
+  exit 1
+fi
+
+TEMP=`mktemp -q "${TMPDIR}"rinker-temp.XXXXXX` # Temp storage file
+if [ $? -ne 0 ]; then
+  echo "$0: Can't create temp storage file, exiting..."
+  exit 1
+fi
+
 LOGLEVEL=2                            # 0=off,1=normal,2=verbose
 LOGMAX=500                            # Max log lines, 0=unlimited
+AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2'
 #################################################################
 
-AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2'
 # get current IP from ip.changeip.com, and store in $TEMP
-wget -q -U "$AGENT" -O $TEMP ip.changeip.com
+wget -q -U "${AGENT}" -O "${TEMP}" ip.changeip.com
 
 # parse $TEMP for the ip, and store in $TMPIP
-grep IPADDR < $TEMP | cut -d= -s -f2 | cut -d- -s -f1 > $TMPIP
-IP=`cat $TMPIP`
+grep IPADDR < "${TEMP}" | cut -d= -s -f2 | cut -d- -s -f1 > "${TMPIP}"
+IP=$(cat "${TMPIP}")
+
+# changeip
+# URL=\"'https://nic.changeip.com/nic/update?cmd=update&set=$HOST&ip=$IP'\"
+
+# google
+# URL=\"'https://domains.google.com/nic/update?hostname=$HOST&myip=$IP'\"
+
+# opendns
+# URL=\"'https://updates.opendns.com/nic/update?hostname=$HOST'\"
+
+IPPATH="${TMPDIR}rinker-ip-storage-${1}"     # IP address storage file
+USER=$(eval "echo $2")
+PASS=$(eval "echo $3")
+HOST=$(eval "echo $4")
+URL=$(eval "echo $5")
 
 # compare $IPPATH with $TMPIP, and if different, execute update
-if diff $IPPATH $TMPIP > /dev/null
+if diff "${IPPATH}" "${TMPIP}" > /dev/null
   then                                # same IP, no update
       if [ $LOGLEVEL -eq 2 ]
         then                          # if verbose, log no change
-          echo "--------------------------------" >> $LOGPATH
-          date >> $LOGPATH
-          echo "No Change" >> $LOGPATH
-          echo -e "IP: \c" >> $LOGPATH
-          cat $IPPATH >> $LOGPATH
+          echo "--------------------------------" >> "${LOGPATH}"
+          date >> "${LOGPATH}"
+          echo "No Change" >> "${LOGPATH}"
+          echo -e "IP: \c" >> "${LOGPATH}"
+          cat "${IPPATH}" >> "${LOGPATH}"
       fi
   else                                # different IP, execute update
-      wget -q -U "$AGENT" -O /dev/null --http-user=$USER --http-password=$PASS "https://nic.changeip.com/nic/update?cmd=update&set=$SET&ip=$IP"
+      wget -q -U "$AGENT" -O /dev/null --no-check-certificate --http-user="${USER}" --http-password="${PASS}" "${URL}"
       if [ $LOGLEVEL -ne 0 ]
         then                          # if logging, log update
-          echo "--------------------------------" >> $LOGPATH
-          date >> $LOGPATH
-          echo "Updating" >> $LOGPATH
-          echo -e "NewIP: \c" >> $LOGPATH
-          cat $TMPIP >> $LOGPATH
+          echo "--------------------------------" >> "${LOGPATH}"
+          date >> "${LOGPATH}"
+          echo "Updating" >> "${LOGPATH}"
+          echo -e "NewIP: \c" >> "${LOGPATH}"
+          cat "${TMPIP}" >> "${LOGPATH}"
           if [ $LOGLEVEL -eq 2 ]
             then                      # verbose logging
-              echo -e "OldIP: \c" >> $LOGPATH
-              cat $IPPATH >> $LOGPATH
-              cat $TEMP >> $LOGPATH   # log the ChangeIP.com update reply
+              echo -e "OldIP: \c" >> "${LOGPATH}"
+              cat "${IPPATH} >>" "${LOGPATH}"
+              cat "${TEMP} >> ""${LOGPATH}"   # log the ChangeIP.com update reply
           fi
       fi
-      cp $TMPIP $IPPATH               # Store new IP
+      cp "${TMPIP}" "${IPPATH}"               # Store new IP
 fi
 
 # if $LOGMAX not equal to 0, reduce log size to last $LOGMAX number of lines
 if [ $LOGMAX -ne 0 ]
   then
-      tail -n $LOGMAX $LOGPATH > $TEMP
-      cp $TEMP $LOGPATH
+      tail -n $LOGMAX "${LOGPATH}" > "${TEMP}"
+      cp "${TEMP}" "${LOGPATH}"
 fi
