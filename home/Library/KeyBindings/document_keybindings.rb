877064ba0b36486179caae278de629d0627cacb8
Andrew Cates <catesandrew@gmail.com>
2015-05-27 10:37:09 -0700

    Output keybindings formatted for Dash

--- home/Library/KeyBindings/document_keybindings.rb
+++ home/Library/KeyBindings/document_keybindings.rb
@@ -10,42 +10,27 @@ infile.close
 date = Time.now.strftime('%m/%d/%Y')
 
 output = ''
-style =<<STYLE
-<style>
-table { margin-bottom:20px; border:none; width: 100%; }
-
-caption { text-align:left; padding:5px; font-weight:bold; border: dotted 1px #777;background:#eee; margin-bottom:10px }
-td,th { font-weight:bold; padding:3px; border: solid 1px #ccc; padding:4px }
-td:nth-child(1),td:nth-child(2),td:nth-child(3) { width:40px}
-td:first-child {font-weight:bold !important}
-td:last-child { font-weight:normal;width:auto }
-
-</style>
-
-STYLE
 
 intro =<<INTRO
-DefaultKeyBindings.dict file (`~/Library/KeyBindings/DefaultKeyBindings.dict`) for Mac OS X, created by [Brett Terpstra][] and based heavily on work done by [Lri][lrikeys].
-Please note that these bindings won't work in all applications: TextWrangler and TextMate, for example, override these with their own settings.
-See Lri's [gists][lrigists] and [website][lriweb] for more coding madness.
+cheatsheet do
+  title 'DefaultKeyBinding'
+  docset_file_name 'DefaultKeyBinding'
+  keyword 'keybinding'
 
-[lrikeys]: http://www.cs.helsinki.fi/u/lranta/keybindings/
-[lriweb]: http://www.cs.helsinki.fi/u/lranta/
-[lrigists]: https://gist.github.com/Lri
-[brett terpstra]: http://brettterpstra.com
-
-<b>Installation</b>: Copy the DefaultKeyBindings.dict file to the `~/Library/KeyBindings/` directory (create `KeyBindings` if it doesn't already exist). 
-Any open applications will need to be re-started before the key bindings will take effect -- or log out and log back in.
+  introduction "DefaultKeyBinding.dict file for Mac OS X, created by [Brett Terpstra](http://brettterpstra.com) and based heavily on work done by [Lri](http://www.cs.helsinki.fi/u/lranta/keybindings/). Please note that these bindings won't work in all applications: TextWrangler and TextMate, for example, override these with their own settings.
+See Lri's [gists](https://gist.github.com/Lri) and [website](http://www.cs.helsinki.fi/u/lranta/) for more coding madness.
 
 <b>Documentation</b> <i>(last updated #{date}.)</i>
 
-*Grouped items begin with the groups shortcut (if exists), followed by a subgroup (if exists) followed by the keys specified.*
+*Grouped items begin with the groups shortcut (if exists), followed by a subgroup (if exists) followed by the keys specified.*"
 
 INTRO
 
 outro =<<OUTRO
-
-This documentation is generated automatically from the comments and commands in the DefaultKeyBinding.dict file. The script `document_keybindings.rb` is free for use, but it's specifically designed for use with my formatting in the bindings plist (i.e. it's a little finicky).
+  notes <<-'END'
+    * Created by [ttscoff](https://github.com/ttscoff/KeyBindings).
+  END
+end
 
 OUTRO
 
@@ -59,11 +44,10 @@ subgroup_command = ''
 subgroup_desc = ''
 desc = ''
 command = ''
-skip = false
 note = ''
 
 def e_sh(str)
-	str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF\n])/, '\\').gsub(/\n/, "'\n'").sub(/^$/, "''")
+	str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\u007F-\u00FF\n])/, '\\').gsub(/\n/, "'\n'").sub(/^$/, "''")
 end
 
 def translate_command(str)
@@ -87,9 +71,9 @@ input.split("\n").each {|line|
     if level == 1
       subgroup_command = ''
       subgroup_desc = ''
-      output += "|||||\n"
+      output += "\n"
     elsif level == 0
-      output += "[ #{group_desc} ]\n\n"
+      output += "  end\n\n"
       group_command = ''
       group_desc = ''
     end
@@ -102,11 +86,11 @@ input.split("\n").each {|line|
     if level == 1
       group_command = translate_command($1)
       group_desc = $2
-      output += "\n|#{group_desc} (#{group_command})||||\n|:----:|:----:|:----:|:----|\n"
+      output += "\n  category do\n    id '#{group_desc} (#{group_command})'\n"
     elsif level == 2
       subgroup_command = translate_command($1)
       subgroup_desc = $2
-      output += "|#{subgroup_desc} (#{subgroup_command})||| |\n"
+      output += "  category do\n    id '#{subgroup_desc} (#{subgroup_command})'\n"
     else
       prefix = $1
     end
@@ -115,26 +99,49 @@ input.split("\n").each {|line|
     command = translate_command($1)
     note = "(#{note})" if note != ''
     if level == 0
-      toplevel.push("|#{command}|#{desc} #{note}|\n")
+
+item =<<ITEM
+      notes <<-END
+        #{note}
+      END
+ITEM
+      tmp  = "    entry do\n"
+      tmp += "      name '#{desc}'\n"
+      tmp += "      command '"
+      tmp += "#{command}".to_s.gsub("'"){"\\'"}
+      tmp += "'\n"
+      tmp += item
+      tmp += "    end\n"
+      toplevel.push(tmp)
     else
       command = prefix + "," + command if prefix
-      output += "|#{group_command} |#{subgroup_command} |#{command} |#{desc} #{note}|\n"
+item =<<ITEM
+      notes <<-END
+        #{note}
+      END
+ITEM
+    output += "    entry do\n"
+    output += "      name '#{desc}'\n"
+    output += "      command '"
+    output += "#{group_command} #{subgroup_command} #{command}".to_s.gsub("'"){"\\'"}
+    output += "'\n"
+    output += item
+    output += "    end\n"
     end
     note = ''
   end
 }
-topoutput = "|General Commands||\n|Key|Function|\n|:----:|:----|\n"
+topoutput = "  category do\n    id 'General Commands'\n"
 toplevel.each {|line|
   topoutput += line
 }
-topoutput += "[ General Commands ]\n\n"
+topoutput += "  end\n\n"
 
-# output = style + topoutput + output
 output = topoutput + output
 
-htmlout = %x{echo #{e_sh output}|/usr/local/bin/multimarkdown}
+htmlout = %x{echo #{e_sh output}}
 
-outfile = File.new('readme.md','w')
+outfile = File.new('keybindings.rb','w')
 outfile.puts intro
 outfile.puts htmlout
 outfile.puts outro
