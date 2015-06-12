1da410e159ff1caccdb71f62a8f4cf6c3d3550c0
Andrew Cates <catesandrew@gmail.com>
2015-06-11 20:34:55 -0700

    Fix for mveditor.sh

--- home/.bash/mveditor.sh
+++ home/.bash/mveditor.sh
@@ -75,4 +75,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux"  ]; then
 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT"  ]; then
     # Do something under Windows NT platform
+    echo "Windows not yet supported"
 fi
+
