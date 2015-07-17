dc7b1346568f67552a23902085b48294df3e98a2
Andrew Cates <catesandrew@gmail.com>
2015-07-16 18:36:06 -0700

    Further tweaking of iterm2 integration

--- home/.bash/plugins/iterm2.plugin.bash
+++ home/.bash/plugins/iterm2.plugin.bash
@@ -5,7 +5,8 @@ if [ "${INSIDE_EMACS:-""}" != "" ]; then
     :
 elif [ "${TERM_PROGRAM:-""}" != "" ]; then
     if ( [ "$TERM_PROGRAM" == "iTerm.app" ] ); then
-    echo "TERM_PROGRAM = $TERM_PROGRAM";
         [ -f ~/.iterm2_shell_integration.bash ] && source ~/.iterm2_shell_integration.bash
     fi
+else
+    [ -f ~/.iterm2_shell_integration.bash ] && source ~/.iterm2_shell_integration.bash
 fi
