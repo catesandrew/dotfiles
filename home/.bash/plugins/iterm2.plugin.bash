105b083a70fba0f6be7ccc48d18d1cfef70b459c
Andrew Cates <catesandrew@gmail.com>
2015-07-16 18:31:02 -0700

    Dont kill ansi term in emacs you sob iterm2 int

--- home/.bash/plugins/iterm2.plugin.bash
+++ home/.bash/plugins/iterm2.plugin.bash
@@ -1,4 +1,11 @@
 cite about-plugin
 about-plugin 'load iterm2, if you are using it'
 
-[ -f ~/.iterm2_shell_integration.bash ] && source ~/.iterm2_shell_integration.bash
+if [ "${INSIDE_EMACS:-""}" != "" ]; then
+    :
+elif [ "${TERM_PROGRAM:-""}" != "" ]; then
+    if ( [ "$TERM_PROGRAM" == "iTerm.app" ] ); then
+    echo "TERM_PROGRAM = $TERM_PROGRAM";
+        [ -f ~/.iterm2_shell_integration.bash ] && source ~/.iterm2_shell_integration.bash
+    fi
+fi
