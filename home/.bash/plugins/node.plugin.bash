2488861f9d81a99920068db7262069825bd71dee
Andrew Cates <catesandrew@gmail.com>
2015-09-28 21:30:33 -0700

    faster npm with JOBS=max

--- home/.bash/plugins/node.plugin.bash
+++ home/.bash/plugins/node.plugin.bash
@@ -5,3 +5,7 @@ pathmunge ./node_modules/.bin
 
 # Make sure the global npm prefix is on the path
 [[ `which npm` ]] && pathmunge $(npm config get prefix)/bin
+
+# tells npm to compile and install all your native addons in parallel and not
+# sequentially. This greatly increases installation times.
+[[ `which npm` ]] && export JOBS=max
