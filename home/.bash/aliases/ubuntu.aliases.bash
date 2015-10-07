a7581e90de93e111d46e979a47cece2f17e85809
Andrew Cates <catesandrew@gmail.com>
2015-10-07 10:29:42 -0700

    Aliases for ubuntu

--- home/.bash/aliases/ubuntu.aliases.bash
+++ home/.bash/aliases/ubuntu.aliases.bash
@@ -3,4 +3,16 @@ if [ $(uname) = "Linux" ]; then
     alias pbcopy=&#039;xclip -selection clipboard&#039;
     alias pbpaste=&#039;xclip -selection clipboard -o&#039;
     alias open=xdg-open
+
+    # Alias for getting OpenPGP keys for Launchpad PPAs on Ubuntu
+    # http://www.commandlinefu.com/commands/view/2437/alias-for-getting-openpgp-keys-for-launchpad-ppas-on-ubuntu
+    alias launchpadkey="sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys"
+
+    # (Debian/Ubuntu) Discover what package a file belongs to
+    # http://www.commandlinefu.com/commands/view/1947/debianubuntu-discover-what-package-a-file-belongs-to
+    # alias dls="dpkg -S /usr/bin/ls"
+
+    # Find distro name and/or version/release
+    # http://www.commandlinefu.com/commands/view/1228/find-distro-name-andor-versionrelease
+    # cat /etc/*-release
 fi
