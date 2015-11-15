281e69d7888e32094211f8e5fd68ea4fdf0b8057
Andrew Cates <catesandrew@gmail.com>
2015-11-15 07:23:25 -0800

    feat: add new tmux aliases

--- home/.bash/aliases/tmux.aliases.bash
+++ home/.bash/aliases/tmux.aliases.bash
@@ -2,5 +2,13 @@ cite 'about-alias'
 about-alias 'Tmux terminal multiplexer'
 
 alias txl='tmux ls'
+# alias txl='tmux list-sessions'
+# alias txl="tmux list-sessions"
+
 alias txn='tmux new -s'
 alias txa='tmux a -t'
+# alias txa='tmux attach'
+# alias txa="tmux attach-session"
+
+alias txc="clear && tmux clear-history"
+alias txk="tmux kill-session -t"
