e436f6221ee6e1bffe77e0f9d378bfb1ab979005
Andrew Cates <catesandrew@gmail.com>
2015-11-15 07:23:44 -0800

    feat: update all npm global packages except npm

--- home/.bash/aliases/npm.aliases.bash
+++ home/.bash/aliases/npm.aliases.bash
@@ -7,5 +7,5 @@ about-alias 'npm abbreviations'
 # than <package>@latest and you want to keep them on the absolute newest
 # version rather than latest.
 
-alias nub='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f3); do npm -g install "$package"; done'
-alias nup='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do npm -g install "$package"; done'
+alias nub='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f3); do IFS='@' read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm -g install "$package"; fi done'
+alias nup='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do IFS='@' read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm -g install "$package"; fi done'
