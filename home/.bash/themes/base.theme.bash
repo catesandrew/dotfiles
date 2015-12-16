78d7a6ecb65ddfd63b9a925b3ddb069c83e37b78
Andrew Cates <catesandrew@gmail.com>
2015-12-16 11:09:56 -0800

    feat: oh-my-git theme

--- home/.bash/themes/base.theme.bash
+++ home/.bash/themes/base.theme.bash
@@ -171,6 +171,67 @@ function git_prompt_vars {
 
   SCM_PREFIX=${GIT_THEME_PROMPT_PREFIX:-$SCM_THEME_PROMPT_PREFIX}
   SCM_SUFFIX=${GIT_THEME_PROMPT_SUFFIX:-$SCM_THEME_PROMPT_SUFFIX}
+
+  ## oh my git
+
+  deteached=
+  just_init=
+  has_upstream=
+  has_modifications=
+  has_modifications_cached=
+  has_adds=
+  has_deletions=
+  has_deletions_cached=
+  ready_to_commit=
+  has_untracked_files=
+  is_on_a_tag=
+  has_diverged=
+  should_push=
+  has_stashes=
+  will_rebase=
+
+  current_commit_hash=$(git rev-parse HEAD 2> /dev/null)
+
+  current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  if [[ $current_branch == 'HEAD' ]]; then detached=true; fi
+
+  local number_of_logs="$(git log --pretty=oneline -n1 2> /dev/null | wc -l)"
+  if [[ $number_of_logs -eq 0 ]]; then
+      just_init=true
+  else
+      local upstream=$(git rev-parse --symbolic-full-name --abbrev-ref @{upstream} 2> /dev/null)
+      if [[ -n "${upstream}" && "${upstream}" != "@{upstream}" ]]; then has_upstream=true; fi
+
+      local git_status="$(git status --porcelain 2> /dev/null)"
+      action="$(get_current_action)"
+
+      if [[ $git_status =~ ($'\n'|^).M ]]; then has_modifications=true; fi
+      if [[ $git_status =~ ($'\n'|^)M ]]; then has_modifications_cached=true; fi
+      if [[ $git_status =~ ($'\n'|^)A ]]; then has_adds=true; fi
+      if [[ $git_status =~ ($'\n'|^).D ]]; then has_deletions=true; fi
+      if [[ $git_status =~ ($'\n'|^)D ]]; then has_deletions_cached=true; fi
+      if [[ $git_status =~ ($'\n'|^)[MAD] && ! $git_status =~ ($'\n'|^).[MAD\?] ]]; then ready_to_commit=true; fi
+
+      local number_of_untracked_files=$(\grep -c "^??" <<< "${git_status}")
+      if [[ $number_of_untracked_files -gt 0 ]]; then has_untracked_files=true; fi
+
+      tag_at_current_commit=$(git describe --exact-match --tags $current_commit_hash 2> /dev/null)
+      if [[ -n $tag_at_current_commit ]]; then is_on_a_tag=true; fi
+
+      if [[ $has_upstream == true ]]; then
+          local commits_diff="$(git log --pretty=oneline --topo-order --left-right ${current_commit_hash}...${upstream} 2> /dev/null)"
+          commits_ahead=$(\grep -c "^<" <<< "$commits_diff")
+          commits_behind=$(\grep -c "^>" <<< "$commits_diff")
+      fi
+
+      if [[ $commits_ahead -gt 0 && $commits_behind -gt 0 ]]; then has_diverged=true; fi
+      if [[ $has_diverged == false && $commits_ahead -gt 0 ]]; then should_push=true; fi
+
+      will_rebase=$(git config --get branch.${current_branch}.rebase 2> /dev/null)
+
+      local number_of_stashes="$(git stash list -n1 2> /dev/null | wc -l)"
+      if [[ $number_of_stashes -gt 0 ]]; then has_stashes=true; fi
+  fi
 }
 
 function svn_prompt_vars {
@@ -357,4 +418,43 @@ function aws_profile {
   else
     echo -e "default"
   fi
+}
+
+# from oh-my-git
+function get_current_action () {
+    local info="$(git rev-parse --git-dir 2>/dev/null)"
+    if [ -n "$info" ]; then
+        local action
+        if [ -f "$info/rebase-merge/interactive" ]
+        then
+            action=${is_rebasing_interactively:-"rebase -i"}
+        elif [ -d "$info/rebase-merge" ]
+        then
+            action=${is_rebasing_merge:-"rebase -m"}
+        else
+            if [ -d "$info/rebase-apply" ]
+            then
+                if [ -f "$info/rebase-apply/rebasing" ]
+                then
+                    action=${is_rebasing:-"rebase"}
+                elif [ -f "$info/rebase-apply/applying" ]
+                then
+                    action=${is_applying_mailbox_patches:-"am"}
+                else
+                    action=${is_rebasing_mailbox_patches:-"am/rebase"}
+                fi
+            elif [ -f "$info/MERGE_HEAD" ]
+            then
+                action=${is_merging:-"merge"}
+            elif [ -f "$info/CHERRY_PICK_HEAD" ]
+            then
+                action=${is_cherry_picking:-"cherry-pick"}
+            elif [ -f "$info/BISECT_LOG" ]
+            then
+                action=${is_bisecting:-"bisect"}
+            fi
+        fi
+
+        if [[ -n $action ]]; then printf "%s" "${1-}$action${2-}"; fi
+    fi
 }
