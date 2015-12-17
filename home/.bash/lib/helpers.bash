4818f1b03c4d75a3a0824f01411026fc6d4e8d0d
Andrew Cates <catesandrew@gmail.com>
2015-12-16 23:59:12 -0800

    chore: remove unused func

--- home/.bash/lib/helpers.bash
+++ home/.bash/lib/helpers.bash
@@ -130,32 +130,6 @@ function brew_reinstall () {
         | while read l; do echo -n "$l "; brew reinstall $l; done
 }
 
-##################################################
-# Fancy PWD display function
-##################################################
-# The home directory (HOME) is replaced with a ~
-# The last pwdmaxlen characters of the PWD are displayed
-# Leading partial directory names are striped off
-# /home/me/stuff          -> ~/stuff               if USER=me
-# /usr/share/big_dir_name -> ../share/big_dir_name if pwdmaxlen=20
-# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x783.html
-##################################################
-bash_prompt_command() {
-    # How many characters of the $PWD should be kept
-    local pwdmaxlen=32
-    # Indicate that there has been dir truncation
-    local trunc_symbol="…"
-    local dir=${PWD##*/}
-    pwdmaxlen=$(( ( pwdmaxlen < ${#dir} ) ? ${#dir} : pwdmaxlen ))
-    NEW_PWD=${PWD/#$HOME/\~}
-    local pwdoffset=$(( ${#NEW_PWD} - pwdmaxlen ))
-    if [ ${pwdoffset} -gt "0" ]
-    then
-        NEW_PWD=${NEW_PWD:$pwdoffset:$pwdmaxlen}
-        NEW_PWD=${trunc_symbol}/${NEW_PWD#*/}
-    fi
-}
-
 # Max length of PWD to display
 MAX_PWD_LENGTH=24
 
