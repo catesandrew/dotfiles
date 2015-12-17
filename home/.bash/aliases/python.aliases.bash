28c2e14976e7e5fa9b3b288b3b3e40ec076737d3
Andrew Cates <catesandrew@gmail.com>
2015-12-17 00:02:53 -0800

    feat: add python pip update alias

---
+++ home/.bash/aliases/python.aliases.bash
@@ -0,0 +1,7 @@
+# Some aliases for python
+
+cite 'about-alias'
+about-alias 'python abbreviations'
+
+# Update Python utilities.
+alias pup='pip install --upgrade setuptools && pip install --upgrade pip'
