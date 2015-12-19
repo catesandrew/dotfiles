7fd718ba0afd6c21b8ba7678571cfdc6948184b4
Andrew Cates <catesandrew@gmail.com>
2015-12-19 10:37:03 -0800

    chore: use dupe core tools gnu ls if avail

--- home/.bash/aliases/dupes.aliases.bash
+++ home/.bash/aliases/dupes.aliases.bash
@@ -7,6 +7,7 @@ if [ $(uname) = "Darwin" ]; then
         if hash ggrep 2>/dev/null; then
             alias grep='ggrep --color'
             alias grepno="ggrep --color=never -n -E '.*'"
+            alias ls='gls --color'
         fi
     fi
 fi
