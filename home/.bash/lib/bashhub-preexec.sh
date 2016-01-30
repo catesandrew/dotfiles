49582d33a8ef7550192684fc0430a912974d036f
Andrew Cates <catesandrew@gmail.com>
2016-01-29 20:20:36 -0800

    add bashhub to shell

---
+++ home/.bash/lib/bashhub-preexec.sh
@@ -0,0 +1,56 @@
+#
+# bashhub.sh
+# Main file that is sourced onto our path for Bash.
+#
+
+# Avoid duplicate inclusion
+if [[ "$__bh_imported" == "defined" ]]; then
+    return 0
+fi
+__bh_imported="defined"
+
+export BH_HOME_DIRECTORY="$HOME/.bashhub/"
+export BH_EXEC_DIRECTORY="$HOME/.bashhub/env/bin"
+
+BH_DEPS_DIRECTORY=${BH_DEPS_DIRECTORY:=$BH_HOME_DIRECTORY/deps}
+
+__bh_setup_bashhub() {
+    # check that we're using bash and that all our
+    # dependencies are satisfied.
+    if [[ -n $BASH_VERSION ]] && \
+       [[ -f $BH_DEPS_DIRECTORY/lib-bashhub.sh ]]; then
+
+        # Pull in our libs
+        source "$BH_DEPS_DIRECTORY/lib-bashhub.sh"
+
+        # Hook bashhub into preexec and precmd.
+        __bh_hook_bashhub
+
+        # Install our tab completion
+        source "$BH_DEPS_DIRECTORY/bashhub_completion_handler.sh"
+    fi
+}
+
+__bh_hook_bashhub() {
+    # Hook into preexec and precmd functions
+    if ! contains_element __bh_preexec "${preexec_functions[@]}"; then
+        preexec_functions+=(__bh_preexec)
+    fi
+
+    if ! contains_element __bh_precmd "${preexec_functions[@]}"; then
+        precmd_functions+=(__bh_precmd)
+        precmd_functions+=(__bh_bash_precmd)
+    fi
+}
+
+__bh_bash_precmd() {
+    if [[ -e $BH_HOME_DIRECTORY/response.bh ]]; then
+        local command=$(head -n 1 "$BH_HOME_DIRECTORY/response.bh")
+        rm "$BH_HOME_DIRECTORY/response.bh"
+        history -s "$command"
+        echo "$command"
+        eval "$command"
+     fi;
+}
+
+__bh_setup_bashhub
--- home/.bash/plugins/bashhub.plugin.bash
+++
@@ -1,6 +0,0 @@
-cite about-plugin
-about-plugin 'load bashhub, if you are using it'
-
-if [ -f $HOME/.bashhub/bashhub.sh ]; then
-    source $HOME/.bashhub/bashhub.sh
-fi
