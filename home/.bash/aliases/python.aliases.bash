0c855d17346554d419e8ecca6ce29219f935ffea
Andrew Cates <catesandrew@gmail.com>
2016-01-29 20:20:49 -0800

    add pip upgrade all alias `pipup`

--- home/.bash/aliases/python.aliases.bash
+++ home/.bash/aliases/python.aliases.bash
@@ -5,3 +5,6 @@ about-alias 'python abbreviations'
 
 # Update Python utilities.
 alias pup='pip install --upgrade setuptools && pip install --upgrade pip'
+
+# http://mikegrouchy.com/blog/2014/06/pro-tip-pip-upgrade-all-python-packages.html
+alias pipup="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U"
