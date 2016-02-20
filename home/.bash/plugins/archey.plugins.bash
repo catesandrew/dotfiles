47a7abf8d1f8b73dcf8288397ac4e7a026f6f61a
Andrew Cates <catesandrew@gmail.com>
2016-02-20 05:28:15 -0800

    fix command args to archey on unix

--- home/.bash/plugins/archey.plugins.bash
+++ home/.bash/plugins/archey.plugins.bash
@@ -1,4 +1,9 @@
-if brew_contains_element "archey" || \
-    hash archey 2>/dev/null; then
+if [ "$__dot_system_type" == "Darwin" ] && \
+     brew_contains_element "archey"; then
   archey -o
+else
+  if brew_contains_element "archey" || \
+      hash archey 2>/dev/null; then
+    archey
+  fi
 fi
