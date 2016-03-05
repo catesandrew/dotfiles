500c219b319f8b5aad0a3af879880dec8386b58f
Andrew Cates <catesandrew@gmail.com>
2016-03-05 06:42:11 -0800

    chore: cleanup bash scripts

--- home/.bash/themes/base.theme.bash
+++ home/.bash/themes/base.theme.bash
@@ -65,9 +65,9 @@ function scm {
 
 function scm_prompt_char {
   if [[ -z $SCM ]]; then scm; fi
-  if [[ $SCM == $SCM_GIT ]]; then SCM_CHAR=$SCM_GIT_CHAR
-  elif [[ $SCM == $SCM_HG ]]; then SCM_CHAR=$SCM_HG_CHAR
-  elif [[ $SCM == $SCM_SVN ]]; then SCM_CHAR=$SCM_SVN_CHAR
+  if [[ $SCM == "$SCM_GIT" ]]; then SCM_CHAR=$SCM_GIT_CHAR
+  elif [[ $SCM == "$SCM_HG" ]]; then SCM_CHAR=$SCM_HG_CHAR
+  elif [[ $SCM == "$SCM_SVN" ]]; then SCM_CHAR=$SCM_SVN_CHAR
   else SCM_CHAR=$SCM_NONE_CHAR
   fi
 }
@@ -77,9 +77,9 @@ function scm_prompt_vars {
   scm_prompt_char
   SCM_DIRTY=0
   SCM_STATE=''
-  [[ $SCM == $SCM_GIT ]] && git_prompt_vars && return
-  [[ $SCM == $SCM_HG ]] && hg_prompt_vars && return
-  [[ $SCM == $SCM_SVN ]] && svn_prompt_vars && return
+  [[ $SCM == "$SCM_GIT" ]] && git_prompt_vars && return
+  [[ $SCM == "$SCM_HG" ]] && hg_prompt_vars && return
+  [[ $SCM == "$SCM_SVN" ]] && svn_prompt_vars && return
 }
 
 function scm_prompt_info {
@@ -87,9 +87,9 @@ function scm_prompt_info {
   scm_prompt_char
   SCM_DIRTY=0
   SCM_STATE=''
-  [[ $SCM == $SCM_GIT ]] && git_prompt_info && return
-  [[ $SCM == $SCM_HG ]] && hg_prompt_info && return
-  [[ $SCM == $SCM_SVN ]] && svn_prompt_info && return
+  [[ $SCM == "$SCM_GIT" ]] && git_prompt_info && return
+  [[ $SCM == "$SCM_HG" ]] && hg_prompt_info && return
+  [[ $SCM == "$SCM_SVN" ]] && svn_prompt_info && return
 }
 
 function git_prompt_vars {
@@ -114,19 +114,55 @@ function git_prompt_vars {
   SCM_COMMITS_BEHIND=0
   SCM_CURRENT_BRANCH=
 
+  export SCM_WILL_REBASE
+  export SCM_ACTION
+  export SCM_HAS_STASHES
+  export SCM_SHOULD_PUSH
+  export SCM_GIT_DETACHED
+  export SCM_HAS_MODIFICATIONS_CACHED
+  export SCM_HAS_ADDS
+  export SCM_HAS_DELETIONS_CACHED
+  export SCM_READY_TO_COMMIT
+  export SCM_HAS_MODIFICATIONS
+  export SCM_HAS_DELETIONS
+  export SCM_HAS_UNTRACKED_FILES
+  export SCM_CURRENT_BRANCH
+  export SCM_HAS_UPSTREAM
+  export SCM_IS_ON_A_TAG
+
+  local status
+  local untracked_count
+  local unstaged_count
+  local staged_count
+  local tracking_info
+  local remote_name
+  local remote_branch
+  local remote_info
+  local num_remotes
+  local REF
+  local ahead_re
+  local behind_re
+  local stash_count
+  local number_of_logs
+  local stash_count
+  local detached_prefix
+  local git_status_flags
+  local branch_gone
+
   SCM_STATE=${GIT_THEME_PROMPT_CLEAN:-$SCM_THEME_PROMPT_CLEAN}
   if [[ "$(git config --get bash-it.hide-status)" != "1" ]]; then
-    [[ "${SCM_GIT_IGNORE_UNTRACKED}" = "true" ]] && local git_status_flags='-uno'
-    local status="$(git status -b --porcelain ${git_status_flags} 2> /dev/null ||
-                    git status --porcelain ${git_status_flags} 2> /dev/null)"
-    if [[ -n "${status}" ]] && [[ "${status}" != "\n" ]] && [[ -n "$(grep -v ^# <<< "${status}")" ]]; then
+    [[ "${SCM_GIT_IGNORE_UNTRACKED}" = "true" ]] && git_status_flags='-uno'
+    status=$(git status -b --porcelain "${git_status_flags}" 2> /dev/null || git status --porcelain "${git_status_flags}" 2> /dev/null)
+    if [[ -n "${status}" ]] && \
+         [[ "${status}" != "\n" ]] && \
+         [[ -n $(grep -v ^# <<< "${status}") ]]; then
       SCM_DIRTY=1
-      if [[ "${SCM_GIT_SHOW_DETAILS}" = "true" ]]; then
-        local untracked_count="$(egrep -c '^\?\? .+' <<< "${status}")"
-        local unstaged_count="$(egrep -c '^.[^ ?#] .+' <<< "${status}")"
-        local staged_count="$(egrep -c '^[^ ?#]. .+' <<< "${status}")"
+      if [[ $SCM_GIT_SHOW_DETAILS = "true" ]]; then
+        untracked_count="$(egrep -c '^\?\? .+' <<< "${status}")"
+        unstaged_count="$(egrep -c '^.[^ ?#] .+' <<< "${status}")"
+        staged_count="$(egrep -c '^[^ ?#]. .+' <<< "${status}")"
 
-        if [[ "${staged_count}" -gt 0 ]]; then
+        if [[ $staged_count -gt 0 ]]; then
           details+=" ${SCM_GIT_STAGED_CHAR}${staged_count}"
           SCM_DIRTY=3
 
@@ -134,12 +170,12 @@ function git_prompt_vars {
           if [[ $status =~ ($'\n'|^)A ]]; then SCM_HAS_ADDS=true; fi
           if [[ $status =~ ($'\n'|^)D ]]; then SCM_HAS_DELETIONS_CACHED=true; fi
 
-          if [[ "${unstaged_count}" -eq 0 ]]; then
+          if [[ $unstaged_count -eq 0 ]]; then
             SCM_READY_TO_COMMIT=true
           fi
         fi
 
-        if [[ "${unstaged_count}" -gt 0 ]]; then
+        if [[ $unstaged_count -gt 0 ]]; then
           details+=" ${SCM_GIT_UNSTAGED_CHAR}${unstaged_count}"
           SCM_DIRTY=2
 
@@ -147,7 +183,7 @@ function git_prompt_vars {
           if [[ $status =~ ($'\n'|^).D ]]; then SCM_HAS_DELETIONS=true; fi
         fi
 
-        if [[ "${untracked_count}" -gt 0 ]]; then
+        if [[ $untracked_count -gt 0 ]]; then
           details+=" ${SCM_GIT_UNTRACKED_CHAR}${untracked_count}"
           SCM_DIRTY=1
 
@@ -160,19 +196,20 @@ function git_prompt_vars {
 
   SCM_CHANGE=$(git rev-parse --short HEAD 2>/dev/null)
 
-  local ref=$(git symbolic-ref -q HEAD 2> /dev/null)
-  if [[ -n "$ref" ]]; then
-    SCM_BRANCH=${SCM_THEME_BRANCH_PREFIX}${ref#refs/heads/}
-    SCM_CURRENT_BRANCH="${ref#refs/heads/}"
-    local tracking_info="$(grep "${SCM_BRANCH}\.\.\." <<< "${status}")"
+  REF=$(git symbolic-ref -q HEAD 2> /dev/null)
+  if [[ -n "$REF" ]]; then
+    SCM_BRANCH=${SCM_THEME_BRANCH_PREFIX}${REF#refs/heads/}
+    SCM_CURRENT_BRANCH=${REF#refs/heads/}
+    tracking_info=$(grep "${SCM_BRANCH}\.\.\." <<< "${status}")
+
     if [[ -n "${tracking_info}" ]]; then
-      [[ "${tracking_info}" =~ .+\[gone\]$ ]] && local branch_gone="true"
-      tracking_info=${tracking_info#\#\# ${SCM_BRANCH}...}
+      [[ "${tracking_info}" =~ .+\[gone\]$ ]] && branch_gone="true"
+      tracking_info=${tracking_info#\#\# "${SCM_BRANCH}"...}
       tracking_info=${tracking_info% [*}
-      local remote_name=${tracking_info%%/*}
-      local remote_branch=${tracking_info#${remote_name}/}
-      local remote_info=""
-      local num_remotes=$(git remote | wc -l 2> /dev/null)
+      remote_name=${tracking_info%%/*}
+      remote_branch=${tracking_info#${remote_name}/}
+      remote_info=""
+      num_remotes=$(git remote | wc -l 2> /dev/null)
 
       if [[ "${num_remotes}" -ge 1 ]]; then
         SCM_HAS_UPSTREAM=true
@@ -197,24 +234,24 @@ function git_prompt_vars {
     fi
     SCM_GIT_DETACHED="false"
   else
-    local detached_prefix=""
-    ref=$(git describe --tags --exact-match 2> /dev/null)
-    if [[ -n "$ref" ]]; then
+    detached_prefix=""
+    REF=$(git describe --tags --exact-match 2> /dev/null)
+    if [[ -n "$REF" ]]; then
       SCM_IS_ON_A_TAG=true
-      tag_at_current_commit="${ref}"
+      tag_at_current_commit="${REF}"
       detached_prefix=${SCM_THEME_TAG_PREFIX}
     else
-      ref=$(git describe --contains --all HEAD 2> /dev/null)
-      ref=${ref#remotes/}
-      [[ -z "$ref" ]] && ref=${SCM_CHANGE}
+      REF=$(git describe --contains --all HEAD 2> /dev/null)
+      REF=${REF#remotes/}
+      [[ -z "$REF" ]] && REF=${SCM_CHANGE}
       detached_prefix=${SCM_THEME_DETACHED_PREFIX}
     fi
-    SCM_BRANCH=${detached_prefix}${ref}
+    SCM_BRANCH=${detached_prefix}${REF}
     SCM_GIT_DETACHED="true"
   fi
 
-  local ahead_re='.+ahead ([0-9]+).+'
-  local behind_re='.+behind ([0-9]+).+'
+  ahead_re='.+ahead ([0-9]+).+'
+  behind_re='.+behind ([0-9]+).+'
   if [[ "${status}" =~ ${ahead_re} ]]; then
     SCM_BRANCH+=" ${SCM_GIT_AHEAD_CHAR}${BASH_REMATCH[1]}"
     SCM_COMMITS_AHEAD=${BASH_REMATCH[1]}
@@ -233,8 +270,8 @@ function git_prompt_vars {
     SCM_SHOULD_PUSH=true
   fi
 
-  local stash_count="$(git stash list 2> /dev/null | wc -l | tr -d ' ')"
-  if [[ "${stash_count}" -gt 0 ]]; then
+  stash_count=$(git stash list 2> /dev/null | wc -l | tr -d ' ')
+  if [[ $stash_count -gt 0 ]]; then
     SCM_BRANCH+=" {${stash_count}}"
     SCM_HAS_STASHES=true
   fi
@@ -245,10 +282,10 @@ function git_prompt_vars {
   SCM_SUFFIX=${GIT_THEME_PROMPT_SUFFIX:-$SCM_THEME_PROMPT_SUFFIX}
 
   ## oh my git
-  local number_of_logs="$(git log --pretty=oneline -n1 2> /dev/null | wc -l)"
+  number_of_logs=$(git log --pretty=oneline -n1 2> /dev/null | wc -l)
   if [[ ${number_of_logs} -gt 0 ]]; then
-    SCM_ACTION="$(get_current_action)"
-    SCM_WILL_REBASE=$(git config --get branch.${SCM_BRANCH}.rebase 2> /dev/null)
+    SCM_ACTION=$(get_current_action)
+    SCM_WILL_REBASE=$(git config --get branch."${SCM_BRANCH}".rebase 2> /dev/null)
   fi
 }
 
@@ -274,16 +311,17 @@ function svn_prompt_vars {
 # - .hg is located in ~/Projects/Foo/.hg
 # - get_hg_root starts at ~/Projects/Foo/Bar and sees that there is no .hg directory, so then it goes into ~/Projects/Foo
 function get_hg_root {
-    local CURRENT_DIR=$(pwd)
+  local CURRENT_DIR
+  CURRENT_DIR=$(pwd)
 
-    while [ "$CURRENT_DIR" != "/" ]; do
-        if [ -d "$CURRENT_DIR/.hg" ]; then
-            echo "$CURRENT_DIR/.hg"
-            return
-        fi
+  while [ "$CURRENT_DIR" != "/" ]; do
+    if [ -d "$CURRENT_DIR/.hg" ]; then
+      echo "$CURRENT_DIR/.hg"
+      return
+    fi
 
-        CURRENT_DIR=$(dirname $CURRENT_DIR)
-    done
+    CURRENT_DIR=$(dirname "$CURRENT_DIR")
+  done
 }
 
 function hg_prompt_vars {
@@ -299,16 +337,16 @@ function hg_prompt_vars {
 
     HG_ROOT=$(get_hg_root)
 
-    if [ -f $HG_ROOT/branch ]; then
+    if [ -f "$HG_ROOT/branch" ]; then
         # Mercurial holds it's current branch in .hg/branch file
-        SCM_BRANCH=$(cat $HG_ROOT/branch)
+        SCM_BRANCH=$(cat "${HG_ROOT}/branch")
     else
         SCM_BRANCH=$(hg summary 2> /dev/null | grep branch: | awk '{print $2}')
     fi
 
-    if [ -f $HG_ROOT/dirstate ]; then
+    if [ -f "${HG_ROOT}/dirstate" ]; then
         # Mercurial holds various information about the working directory in .hg/dirstate file. More on http://mercurial.selenic.com/wiki/DirState
-        SCM_CHANGE=$(hexdump -n 10 -e '1/1 "%02x"' $HG_ROOT/dirstate | cut -c-12)
+        SCM_CHANGE=$(hexdump -n 10 -e '1/1 "%02x"' "${HG_ROOT}/dirstate" | cut -c-12)
     else
         SCM_CHANGE=$(hg summary 2> /dev/null | grep parent: | awk '{print $2}')
     fi
@@ -317,7 +355,7 @@ function hg_prompt_vars {
 function rvm_version_prompt {
   if hash rvm 2> /dev/null; then
     rvm=$(rvm tools identifier) || return
-    if [ $rvm != "system" ]; then
+    if [ "$rvm" != "system" ]; then
       echo -e "$RVM_THEME_PROMPT_PREFIX$rvm$RVM_THEME_PROMPT_SUFFIX"
     fi
   fi
@@ -326,8 +364,8 @@ function rvm_version_prompt {
 function rbenv_version_prompt {
   if hash rbenv 2> /dev/null; then
     rbenv=$(rbenv version-name) || return
-    $(rbenv commands | grep -q gemset) && gemset=$(rbenv gemset active 2> /dev/null) && rbenv="$rbenv@${gemset%% *}"
-    if [ $rbenv != "system" ]; then
+    rbenv commands | grep -q gemset && gemset=$(rbenv gemset active 2> /dev/null) && rbenv="$rbenv@${gemset%% *}"
+    if [ "$rbenv" != "system" ]; then
       echo -e "$RBENV_THEME_PROMPT_PREFIX$rbenv$RBENV_THEME_PROMPT_SUFFIX"
     fi
   fi
@@ -360,7 +398,7 @@ function ruby_version_prompt {
 
 function virtualenv_prompt {
   if [[ -n "$VIRTUAL_ENV" ]]; then
-    virtualenv=`basename "$VIRTUAL_ENV"`
+    virtualenv=$(basename "$VIRTUAL_ENV")
     echo -e "$VIRTUALENV_THEME_PROMPT_PREFIX$virtualenv$VIRTUALENV_THEME_PROMPT_SUFFIX"
   fi
 }
@@ -440,7 +478,8 @@ function aws_profile {
 
 # from oh-my-git
 function get_current_action () {
-  local info="$(git rev-parse --git-dir 2>/dev/null)"
+  local info
+  info="$(git rev-parse --git-dir 2>/dev/null)"
   if [ -n "$info" ]; then
     local action
     if [ -f "$info/rebase-merge/interactive" ]
@@ -500,11 +539,12 @@ function rootable_limited_pwd() {
   local final_max_length="${5:-0}"
   local relative_pwd=''
   local end_basename=''
+  local offset
 
   # treat root dirs as starting point of filesystem, like npm roots
   if [[ ! -z "${root_dir}" ]]; then
-    local offset=${#root_dir}
-    if [ $offset -gt "0" ]; then
+    offset="${#root_dir}"
+    if [ "$offset" -gt "0" ]; then
       end=${end:$offset}
     fi
   elif [[ "$end" =~ $HOME ]]; then
