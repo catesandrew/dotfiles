13cf6d7d765f7620cbb773396921bd0ddfd039c9
Andrew Cates <catesandrew@gmail.com>
2016-04-06 22:09:25 -0700

    feat: add fzf ** completions

--- home/.bash/plugins/fzf.plugin.bash
+++ home/.bash/plugins/fzf.plugin.bash
@@ -12,6 +12,15 @@ if brew_contains_element "fzf"; then
     # Setting ag as the default source for fzf
     export FZF_DEFAULT_COMMAND='ag -l -g ""'
 
+    # view what devices a running process has open `lsof -p 5051`
+    complete -o bashdefault -o default -o nospace -F _fzf_complete_kill lsof
+    # Viewing memory allocation with pmap (vmmap on osx). You can view the
+    # memory allocations for a particular process with pmap (vmmap):
+    complete -o bashdefault -o default -o nospace -F _fzf_complete_kill vmmap
+    complete -o bashdefault -o default -F _fzf_path_completion ec
+    complete -F _fzf_path_completion em
+    complete -F _fzf_path_completion gittower
+
 elif hash fzf 2>/dev/null; then
     true
 fi
