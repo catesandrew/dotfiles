3ba5d4c0faf5ad7e308b32862b61b496a9a78ca4
Andrew Cates <catesandrew@gmail.com>
2016-08-19 11:34:54 -0700

    fix: GOPATH env variable, not GO_PATH

--- home/.bash/plugins/go.plugin.bash
+++ home/.bash/plugins/go.plugin.bash
@@ -3,6 +3,6 @@
 # export GOROOT=$(brew --prefix go)
 if brew_contains_element "go" || \
     hash go 2>/dev/null; then
-    export GOROOT="${__dot_brew_home}/opt/go"
-    export GO_PATH="${__dot_brew_home}/go"
+    export GOROOT="${__dot_brew_home}/opt/go/libexec"
+    export GOPATH="${__dot_brew_home}/go"
 fi
