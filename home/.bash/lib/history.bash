c1a13cf13de39632b197af4207ab84180eaa856d
Andrew Cates <catesandrew@gmail.com>
2016-12-07 15:48:19 -0800

    change histfile location

--- home/.bash/lib/history.bash
+++ home/.bash/lib/history.bash
@@ -33,6 +33,10 @@ HISTIGNORE="npm +(ls|install|view|update):ncu -+(a):cd -:mvim .:em .:* --+(h|he|
 # Useful timestamp format
 HISTTIMEFORMAT='%F %T '
 
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+
 export AUTOFEATURE=true autotest
 
 function rh {
