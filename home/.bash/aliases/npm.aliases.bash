5ab0eeb91d264d17fcd1b14c08d09a3c1752f35c
Andrew Cates <catesandrew@gmail.com>
2016-12-15 22:24:54 -0800

    nup to not update symlinks anymore

--- home/.bash/aliases/npm.aliases.bash
+++ home/.bash/aliases/npm.aliases.bash
@@ -1,8 +1,5 @@
 # Some aliases for npm
 
-# Use `nup` most of the time; use `nub` if you have packages that are newer
-# than <package>@latest and you want to keep them on the absolute newest
-# version rather than latest.
+# alias nup='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do IFS='@' read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm -g install "$package"; fi done'
 
-alias nub='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f3); do IFS='@' read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm -g install "$package"; fi done'
-alias nup='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do IFS='@' read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm -g install "$package"; fi done'
+nup() { for package in $(\ls -F "${NPM_CONFIG_PREFIX}/lib/node_modules" | sed -n 's/[^@]$//p' | xargs npm outdated --no-color -pg --depth=0 | awk '{gsub(/\/.*\//,"",$1); print}' | \grep -o ":.*:" | sed 's/.$//; s/^.//'); do IFS=@ read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm install -g "${package}"; fi done }
