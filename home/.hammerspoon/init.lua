06bd7bfd415a0c68ef9b1bc53f23667d4848a195
Andrew Cates <catesandrew@gmail.com>
2017-01-21 10:35:59 -0800

    change scanner name

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -329,7 +329,7 @@ end
 local usbWatcher = nil
 
 function usbDeviceCallback(data)
-  if (data["productName"] == "ScanSnap S1500M") then
+  if (data["productName"] == "ScanSnap iX500") then
     if (data["eventType"] == "added") then
       hs.application.launchOrFocus("ScanSnap Manager")
     elseif (data["eventType"] == "removed") then
