80d5dd8faf726792152ed9a56fee1b7e37c7e367
Andrew Cates <catesandrew@gmail.com>
2017-01-27 13:20:43 -0800

    improve docker aliases

--- home/.bash/aliases/docker.aliases.bash
+++ home/.bash/aliases/docker.aliases.bash
@@ -75,7 +75,7 @@ drmf() {
     local _ps
     _ps="$(__docker_c ps -a -q)"
 
-    echo "stop and remove (all containers): $_ps"
+    echo "Stop and remove (all containers): $_ps"
     read -p "Are you sure? " -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -91,7 +91,14 @@ __docker_complete dbuild _docker_build
 
 # Dockerfile build, e.g., $dbu tcnksm/test
 dbu() {
-    __docker_c build --tag="$1" .
+    if [ $# -eq 0 ]; then
+        local _name=$(basename "$PWD")
+         __docker_c build --tag="$_name" .
+    else
+        __docker_c build --tag="$1" .
+    fi
+
+    # __docker_c build --tag="$1" .
 }
 __docker_complete dbu __docker_complete_image_repos_and_tags
 
@@ -132,7 +139,14 @@ __docker_complete dexec _docker_exec
 
 # enter into a running container
 dent() {
-    __docker_c exec --interactive --tty "$1" /bin/bash
+    if [ $# -eq 0 ]; then
+        local _name=$(basename "$PWD")
+         __docker_c exec --interactive --tty "$_name" /bin/bash
+    else
+        __docker_c exec --interactive --tty "$1" /bin/bash
+    fi
+
+    # __docker_c exec --interactive --tty "$1" /bin/bash
 }
 __docker_complete dent __docker_complete_containers_running
 
@@ -150,10 +164,10 @@ dh() {
 __docker_complete dh _docker_history
 
 # List images
-di() {
+dls() {
     __docker_c images "$@"
 }
-__docker_complete di _docker_images
+__docker_complete dls _docker_images
 
 # Import the contents from a tarball to create a filesystem image
 dimp() {
@@ -163,16 +177,20 @@ __docker_complete dimp _docker_import
 
 # Get container IP
 dip() {
-    # docker --tls inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
     __docker_c inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
 }
 __docker_complete dip __docker_complete_containers_all
 
 # Return low-level information on a container, image or task
-din() {
-    __docker_c inspect "$@"
+di() {
+    if [ $# -eq 0 ]; then
+        local _name=$(basename "$PWD")
+        __docker_c inspect "$_name"
+    else
+        __docker_c inspect "$@"
+    fi
 }
-__docker_complete din _docker_inspect
+__docker_complete di _docker_inspect
 
 # Kill one or more running containers
 dkill() {
@@ -260,27 +278,33 @@ __docker_complete drmi _docker_rmi
 
 # Remove image specified by $1 or remove all untagged images
 dri() {
-    if [ $# -eq 0 ] ; then
-        # This works by using rmi with a list of image ids. To get the image
-        # ids we call docker images then pipe it to grep "^<none>". The grep
-        # will filter it down to only lines with the value "<none>" in the
-        # repository column. Then to extract the id out of the third column we
-        # pipe it to awk "{print $3}" which will print the third column of each
-        # line passed to it.
-        local images
-        images="$(__docker_c images | grep "^<none>" | awk '{print $3}')"
-
-        echo "Remove untagged images: $images"
-        read -p "Are you sure? " -n 1 -r
-        echo    # (optional) move to a new line
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            __docker_c rmi "$images"
-        fi
+    if [ $# -eq 0 ]; then
+        local _name=$(basename "$PWD")
+        __docker_c rmi "$_name"
     else
         __docker_c rmi "$@"
     fi
 }
 __docker_complete dri _docker_rmi
+
+# Remove all untagged images
+driu() {
+   # This works by using rmi with a list of image ids. To get the image
+   # ids we call docker images then pipe it to grep "^<none>". The grep
+   # will filter it down to only lines with the value "<none>" in the
+   # repository column. Then to extract the id out of the third column we
+   # pipe it to awk "{print $3}" which will print the third column of each
+   # line passed to it.
+   local images
+   images="$(__docker_c images | grep "^<none>" | awk '{print $3}')"
+
+   echo "Remove all untagged images: $images"
+   read -p "Are you sure? " -n 1 -r
+   echo    # (optional) move to a new line
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+       __docker_c rmi "$images"
+   fi
+}
 
 # Remove all images
 drif() {
@@ -297,7 +321,12 @@ drif() {
 
 # Run a command in a new container
 drun() {
-    __docker_c run "$@"
+    if [ $# -eq 0 ]; then
+        local _name=$(basename "$PWD")
+         __docker_c run "$_name"
+    else
+        __docker_c run "$@"
+    fi
 }
 __docker_complete drun _docker_run
 
@@ -323,7 +352,14 @@ __docker_complete dki __docker_complete_images
 # dbash is particularly useful when diagnosing a failed `docker build`. Just
 # dbash the last generated image and re-run the failed command
 dbash() {
-    __docker_c run --rm --interactive --tty --env TERM=xterm --entrypoint /bin/bash "$1"
+    if [ $# -eq 0 ]; then
+        local _name=$(basename "$PWD")
+        __docker_c run --rm --interactive --tty --env TERM=xterm --entrypoint /bin/bash "$_name"
+    else
+        __docker_c run --rm --interactive --tty --env TERM=xterm --entrypoint /bin/bash "$1"
+    fi
+
+    # __docker_c run --rm --interactive --tty --env TERM=xterm --entrypoint /bin/bash "$1"
 }
 __docker_complete dbash  __docker_complete_images
 
@@ -350,6 +386,38 @@ dstats() {
     __docker_c stats "$@"
 }
 __docker_complete dstats _docker_stats
+
+dsudo() {
+  if [ $# -eq 0 ]; then
+    local _name=$(basename "$PWD")
+    __docker_c run -it --rm --privileged --net=host --pid=host -v /:/host debian:sid chroot /host "$_name"
+  else
+    __docker_c run -it --rm --privileged --net=host --pid=host -v /:/host debian:sid chroot /host "$@"
+  fi
+}
+
+dx11() {
+  local name sound x11
+
+  sound=(
+	  --device /dev/snd
+  )
+
+  x11=(
+	  -v /tmp/.X11-unix:/tmp/.X11-unix:ro
+	  -e DISPLAY
+	  --net host # in case of DISPLAY=localhost:*
+	  -v ~/.Xauthority:/home/user/.Xauthority:ro
+	  --ipc host # DevShm errors...
+  )
+
+  if [ $# -eq 0 ]; then
+    _name=$(basename "$PWD")
+    __docker_c run "${x11[@]}" "${sound[@]}" "$_name"
+  else
+    __docker_c run "${x11[@]}" "${sound[@]}" "$@"
+  fi
+}
 
 # Manage Docker Swarm
 dswarm() {
@@ -382,10 +450,38 @@ dupdate() {
 __docker_complete dupdate _docker_update
 
 # Manage Docker volumes
-dvol() {
+dv() {
     __docker_c volume "$@"
 }
-__docker_complete dvol _docker_volume
+__docker_complete dv _docker_volume
+
+# Create a Volume
+dvc() {
+    __docker_c volume create "$@"
+}
+__docker_complete dvc _docker_volume_create
+
+# Display detailed info on one or more volumes
+dvi() {
+    __docker_c volume inspect "$@"
+}
+__docker_complete dvi _docker_volume_inspect
+
+# list volumes
+dvls() {
+    __docker_c volume ls "$@"
+}
+__docker_complete dvls _docker_volume_ls
+
+dvcleanup() {
+  __docker_c volume rm $(__docker_c volume ls -qf dangling=true)
+}
+
+# remove one or more volumes
+dvrm() {
+    __docker_c volume rm "$@"
+}
+__docker_complete dvrm _docker_volume_rm
 
 # Block until a container stops, then print its exit code
 dwait() {
@@ -396,5 +492,209 @@ __docker_complete dwait _docker_wait
 # Show all alias related docker
 dalias() {
     alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort;
+}
+
+dclean() {
+  local dockerInfo dockerRoot dockerPs dockerVolumes dockerUntaggedImages
+  dockerInfo="$(__docker_c -D info)"
+  dockerRoot="$(echo "$dockerInfo" | awk -F ': ' '$1 == "Docker Root Dir" { print $2; return }')"
+  : "${dockerRoot:=/var/lib/docker}"
+
+  dockerPs=( $(__docker_c ps -aq) )
+  if [ ${#dockerPs[@]} -gt 0 ]; then
+	  (
+      echo "PS: "
+      echo "${dockerPs[@]}"
+		  # df -hT "$dockerRoot"
+		  # df -hTi "$dockerRoot"
+		  # __docker_c rm "${dockerPs[@]}" || true
+		  # df -hT "$dockerRoot"
+		  # df -hTi "$dockerRoot"
+	  )
+  fi
+
+  dockerVolumes=( $(__docker_c volume ls -q) )
+  if [ ${#dockerVolumes[@]} -gt 0 ]; then
+	  (
+      echo "VOLUMES:"
+      echo "${dockerVolumes[@]}"
+      echo ""
+		  # df -hT "$dockerRoot"
+		  # df -hTi "$dockerRoot"
+		  # __docker_c volume rm "${dockerVolumes[@]}" || true
+		  # df -hT "$dockerRoot"
+		  # df -hTi "$dockerRoot"
+	  )
+  fi
+
+  dockerUntaggedImages=( $(__docker_c images -q --filter 'dangling=true') )
+  if [ ${#dockerUntaggedImages[@]} -gt 0 ]; then
+	  (
+      echo "UNTAGGED IMAGES:"
+      echo "${dockerUntaggedImages[@]}"
+      echo ""
+		  # df -hT "$dockerRoot"
+		  # df -hTi "$dockerRoot"
+		  # __docker_c rmi "${dockerUntaggedImages[@]}" || true
+		  # df -hT "$dockerRoot"
+		  # df -hTi "$dockerRoot"
+	  )
+  fi
+}
+
+dhubtags() {
+  local jqExpression self page repo nextPage
+  jqExpression='.results[].name'
+  self="dhubtags"
+
+  usage() {
+    cat <<-EOUSAGE
+
+    usage: $self [options] repo [repo ...]
+
+       ie: $self library/docker
+
+           $self library/docker tianon/docker-master | sort -uV
+
+           $self -j '.results[]' tianon/syncthing
+
+           $self -j '.results[] | [ .name, (.full_size | tostring), .last_updated ] | join("\\t")' tianon/syncthing | sort -V | column -ts \$'\\t'
+
+    options:
+      -j='$jqExpression'
+        modify the "j" expression used to extract results
+
+EOUSAGE
+  }
+
+  if [ "$#" -eq 0 ]; then
+    echo >&2 'error: expected at lease one "repo" argument'
+    usage >&2
+    return 1
+  fi
+
+  OPTIND=1
+  while getopts "hj:" opt "$@"; do
+    case $opt in
+      j)
+        jqExpression="$OPTARG";;
+      h)
+        usage >&2
+        return 0
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        usage;
+        return 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument." >&2
+        return 1
+        ;;
+    esac
+  done
+  shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
+
+  _all() {
+    repo="$1"; shift 1
+
+    nextPage="https://hub.docker.com/v2/repositories/${repo}/tags/?page_size=100"
+    while true; do
+      page="$(curl -fsSL "$nextPage")"
+
+      [ "$(echo "$page" | jq --raw-output '.results | length')" -gt 0 ] || break
+      echo "$page" | jq --raw-output "$jqExpression"
+
+      nextPage="$(echo "$page" | jq --raw-output '.next')"
+      [ "$nextPage" != 'null' ] || break
+    done
+  }
+
+  while [ "$#" -gt 0 ]; do
+	  repo="$1"; shift 1
+	  _all "$repo"
+  done
+}
+
+dsshbg() {
+  local dockerArgs before sawDash
+
+  usage() {
+    cat <<-EOUSAGE
+
+ usage: dsshbg [docker args --] [ssh args]
+
+        dsshbg --name ssh-some-server -- me@some.server -o ControlMaster=yes
+          in ~/.ssh/config:
+            Match all
+                # use sockets if they're available
+                ControlPath ~/.ssh/sockets/%r@%h-%p
+
+        dsshbg --name docker-some-server --expose 2375 -- me@some.server -L 0.0.0.0:2375:/var/run/docker.sock
+        docker -H tcp://docker-some-server.docker:2375 info
+
+EOUSAGE
+  }
+
+  if [ "$#" -eq 0 ]; then
+    echo >&2 'error: expected at lease one "repo" argument'
+    usage >&2
+    return 1
+  fi
+
+  dockerArgs=(
+    -d #-it
+    --restart always
+    -v "$PWD:/host/$PWD"
+    -w "/host/$PWD"
+    -v "$HOME:/host/$HOME"
+    -e HOME="/host/$HOME"
+    -v "$HOME/.ssh:$HOME/.ssh" # uhh, ssh(1) is silly (doesn't respect HOME)
+    # Docker reads /etc/passwd and /etc/group before mounting volumes
+    -u "$(id -u):$(id -g)"
+    $(id -G | xargs -n1 echo --group-add)
+    -v /etc/passwd:/etc/passwd:ro
+    -v /etc/group:/etc/group:ro
+    -v /etc/localtime:/etc/localtime:ro
+    -e HISTSIZE -e HISTFILE -e HISTFILESIZE -e HISTCMD -e HISTCONTROL
+    --tmpfs /tmp
+  )
+
+  before=()
+  sawDash=
+  while [ "$#" -gt 0 ]; do
+    i="$1"; shift
+    if [ "$i" = '--' ]; then
+      sawDash=1
+      break
+    fi
+    before+=( "$i" )
+  done
+
+  if [ "$sawDash" ]; then
+    dockerArgs+=( "${before[@]}" )
+  else
+    set -- "${before[@]}"
+  fi
+
+  exec __docker_c run "${dockerArgs[@]}" buildpack-deps:sid-scm ssh -N "$@"
+}
+
+dstale() {
+  local containers
+  IFS=$'\n'
+  containers=( $(__docker_c ps --all --quiet --no-trunc) )
+  unset IFS
+
+  for container in "${containers[@]}"; do
+	  name="$(__docker_c inspect --type container --format '{{.Name}}' "$container")"
+	  name="${name#/}"
+	  imageId="$(__docker_c inspect --type container --format '{{.Image}}' "$container")"
+	  image="$(__docker_c inspect --type container --format '{{.Config.Image}}' "$container")"
+	  imageImageId="$(__docker_c inspect --type image --format '{{.Id}}' "$image")"
+	  if [ "$imageId" != "$imageImageId" ]; then
+		  echo "- $name ($image)"
+	  fi
+  done
 }
 
