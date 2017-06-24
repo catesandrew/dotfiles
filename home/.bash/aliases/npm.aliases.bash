198c6bbab76bcfd1111a1132f39ef230a6f02127
Andrew Cates <catesandrew@gmail.com>
2017-06-24 06:14:11 -0700

    add npm aliases

--- home/.bash/aliases/npm.aliases.bash
+++ home/.bash/aliases/npm.aliases.bash
@@ -2,7 +2,18 @@
 
 # alias nup='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do IFS='@' read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm -g install "$package"; fi done'
 
-nup() { for package in $(\ls -F "${NPM_CONFIG_PREFIX}/lib/node_modules" | sed -n 's/[^@]$//p' | xargs npm outdated --no-color -pg --depth=0 | awk '{gsub(/\/.*\//,"",$1); print}' | \grep -o ":.*:" | sed 's/.$//; s/^.//'); do IFS=@ read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm install -g "${package}"; fi done }
+nup() { NPM_CONFIG_PREFIX="${NVM_DIR}/versions/node/v${NVM_VERSION}"; for package in $(\ls -F "${NPM_CONFIG_PREFIX}/lib/node_modules" | sed -n 's/[^@]$//p' | xargs npm outdated --no-color -pg --depth=0 | awk '{gsub(/\/.*\//,"",$1); print}' | \grep -o ":.*:" | sed 's/.$//; s/^.//'); do IFS=@ read -ra splits <<< "${package}" && if [ "${splits[0]}" != "npm" ]; then npm install -g "${package}"; fi done }
+
+# Get old globally linked repos
+# links=( $(\ls -F "${NVM_DIR}/versions/node/v4.7.0/lib/node_modules" | sed -n 's/[@]$//p') )
+# for i in ${!links[@]}; do l=${links[$i]} && printf "%s\t%s\n" $i $l && ln -snf $(readlink ${NVM_DIR}/versions/node/v4.7.0/lib/node_modules/$l) ${NVM_DIR}/versions/node/v6.10.3/lib/node_modules/$l; done
+
+# Get old globally installed repos
+# repos=( $(\ls -F "${NVM_DIR}/versions/node/v4.7.0/lib/node_modules" | sed -n 's/[^@]$//p' | sed -n 's/$npm$//p' | sed -n 's/$@npm$//p') )
+# repos=( $(\ls -F "${NVM_DIR}/versions/node/v4.7.0/lib/node_modules" | gsed -n 's/[^@]$//p' | awk '!/^npm$/' | awk '!/^@npm$/') )
+# echo ${repos[@]}
+# Reinstall repos
+# for i in ${!repos[@]}; do r=${repos[$i]} && printf "%s\t%s\n" $i $r && npm install -g $r; done
 
 __npm_func_wrap () {
   local cur words cword prev
@@ -22,6 +33,46 @@ __npm_c() {
 }
 
 _completion_loader npm
+
+# npm reinstall
+nri() {
+  if [ -f package.json ]; then
+    no_lock=1
+    if [ -f package-lock.json ]; then
+      rm package-lock.json
+      no_lock=0
+    fi
+
+    if [ -d node_modules ]; then
+      rm -rf node_modules
+    fi
+
+    __npm_c install
+
+    if [ $no_lock ] && [ -f package-lock.json ]; then
+      rm package-lock.json
+    fi
+  fi
+}
+
+ni() {
+  if [ -f package.json ]; then
+    no_lock=1
+    if [ -f package-lock.json ]; then
+      no_lock=0
+    fi
+
+    if [ $# -eq 0 ]; then
+      __npm_c install
+    else
+      __npm_c install "$@"
+    fi
+
+    if [ $no_lock ] && [ -f package-lock.json ]; then
+      rm package-lock.json
+    fi
+  fi
+}
 
 ni() { __npm_c install "$@"; }
 __npm_complete ni npm
@@ -46,6 +97,7 @@ nn() { __npm_c view "$@"; }
 alias np='npm publish'
 alias nr='npm run'
 alias nrb='npm rebuild'
+alias nrc='npm run compile'
 
 nt() { __npm_c test "$@"; }
 # __npm_complete nt npm
