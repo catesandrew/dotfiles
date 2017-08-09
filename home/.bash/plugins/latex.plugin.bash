bf7764b13a6211f607521fa186f0a27db72b518a
Andrew Cates <catesandrew@gmail.com>
2017-08-08 17:12:42 -0700

    fix latex bash plugin

--- home/.bash/plugins/latex.plugin.bash
+++ home/.bash/plugins/latex.plugin.bash
@@ -1,7 +1,7 @@
 # use mactex
 
 # add mactex to the path if its present
-MACTEX_PATH=/usr/local/texlive/2015/bin/x86_64-darwin
+MACTEX_PATH=/usr/local/texlive/2017/bin/x86_64-darwin
 if [[ -d  $MACTEX_PATH ]]; then
     path_munge "${MACTEX_PATH}" "after"
 fi
