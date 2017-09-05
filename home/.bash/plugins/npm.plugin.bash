d96048db50c49fc347b48e15a66e654bc76fe5fa
Andrew Cates <catesandrew@gmail.com>
2017-09-05 10:17:05 -0700

    disable npm plugin

--- home/.bash/plugins/npm.plugin.bash
+++ home/.bash/plugins/npm.plugin.bash
@@ -1,8 +1,8 @@
 # use NPM
 
 # this is the root folder where all globally installed node packages will go
-export NPM_PACKAGES="/usr/local/npm_packages"
-export NODE_PATH="${NPM_PACKAGES}/lib/node_modules:${NODE_PATH}"
+# export NPM_PACKAGES="/usr/local/npm_packages"
+# export NODE_PATH="${NPM_PACKAGES}/lib/node_modules:${NODE_PATH}"
 
 # Add to PATH
 # PATH="./bin:./node_modules/.bin:${NPM_PACKAGES}/bin:${NVM_DIR}/versions/node/v${NVM_VERSION}/bin:${PATH}"
