85f2374ffed68facb84faade35d55f112258d3d3
Andrew Cates <catesandrew@gmail.com>
2017-09-19 05:15:52 -0700

    underbar to space

--- home/.bin/clean-filename.sh
+++ home/.bin/clean-filename.sh
@@ -2,10 +2,10 @@
 
 # convert all spaces to hyphens, remove characters like {}(),\! and convert the filename to lowercase.
 find . -mindepth 1 -not \( -wholename "./.git" -prune \) | while ifs= read -r file; do
-  mv "$file" "$(echo "$file" | tr ' ' '-' | tr -d '[{}(),\!]' | tr -d "\'" | tr '[A-Z]' '[a-z]' | sed 's/_-_/_/g' | sed 's/&/and/g')"
+  mv "$file" "$(echo "$file" | tr '_' ' ' | tr -d '[{}(),\!]' | tr -d "\'" | tr '[A-Z]' '[a-z]' | sed 's/_-_/_/g' | sed 's/&/and/g')"
 done
 
 # just spaces to dashes
 find . -mindepth 1 -not \( -wholename "./.git" -prune \) | while ifs= read -r FILE; do
-  mv "$FILE" "$(echo "$FILE" | tr ' ' '-')"
+  mv "$FILE" "$(echo "$FILE" | tr '_' ' ')"
 done
