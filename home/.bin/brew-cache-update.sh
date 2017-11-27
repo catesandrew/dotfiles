28431230a0b27394a541c3d73a1c783cd1e12f24
Andrew Cates <catesandrew@gmail.com>
2017-11-27 09:17:39 -0800

    safeguard cache

--- home/.bin/brew-cache-update.sh
+++ home/.bin/brew-cache-update.sh
@@ -5,30 +5,39 @@ function add_on_exit() {
 
 . "${BASH_IT}/lib/funcs.sh"
 
-OUTPUT=${HOME}/.brew-init-bash
-s=""
+if hash brew 2>/dev/null; then
+  if [ -f ${HOME}/.brew-init-bash ]; then
+    OUTPUT=${HOME}/.brew-init-bash
+  else
+    OUTPUT=""
+  fi
 
-BREW_HOME="$(brew --prefix)"
-echo "BREW_HOME=${BREW_HOME}" > "${OUTPUT}"
-echo "export BREW_HOME" >> "${OUTPUT}"
+  s=""
 
-__dot_brew_list=($(brew list | sed 's/:.*//'))
-__serialise_to s "${__dot_brew_list[@]}"
+  BREW_HOME="$(brew --prefix)"
+  echo "BREW_HOME=${BREW_HOME}" > "${OUTPUT}"
+  echo "export BREW_HOME" >> "${OUTPUT}"
 
-echo "__dot_brew_list=\"$s\"" >> "${OUTPUT}"
-echo "export __dot_brew_list" >> "${OUTPUT}"
+  __dot_brew_list=($(brew list | sed 's/:.*//'))
+  __serialise_to s "${__dot_brew_list[@]}"
 
-__dot_brew_taps=($(brew tap | sed 's/:.*//'))
-__serialise_to s "${__dot_brew_taps[@]}"
+  echo "__dot_brew_list=\"$s\"" >> "${OUTPUT}"
+  echo "export __dot_brew_list" >> "${OUTPUT}"
 
-echo "__dot_brew_taps=\"$s\"" >> "${OUTPUT}"
-echo "export __dot_brew_taps" >> "${OUTPUT}"
+  __dot_brew_taps=($(brew tap | sed 's/:.*//'))
+  __serialise_to s "${__dot_brew_taps[@]}"
 
-__dot_cask_list=($(brew cask list | sed 's/:.*//'))
-__serialise_to s "${__dot_cask_list[@]}"
+  echo "__dot_brew_taps=\"$s\"" >> "${OUTPUT}"
+  echo "export __dot_brew_taps" >> "${OUTPUT}"
 
-echo "__dot_cask_list=\"$s\"" >> "${OUTPUT}"
-echo "export __dot_cask_list" >> "${OUTPUT}"
+  if [[ $(brew cask --version 2>/dev/null) ]]; then
+    __dot_cask_list=($(brew cask list | sed 's/:.*//'))
+    __serialise_to s "${__dot_cask_list[@]}"
 
-unset s
-unset OUTPUT
+    echo "__dot_cask_list=\"$s\"" >> "${OUTPUT}"
+    echo "export __dot_cask_list" >> "${OUTPUT}"
+  fi
+
+  unset s
+  unset OUTPUT
+fi
