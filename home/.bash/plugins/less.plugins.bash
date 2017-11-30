29d58b9116f7f104811716d4c538d303825375ea
Andrew Cates <catesandrew@gmail.com>
2017-11-30 06:23:41 -0800

    ubuntu fixes

--- home/.bash/plugins/less.plugins.bash
+++ home/.bash/plugins/less.plugins.bash
@@ -1,3 +1,6 @@
 if brew_contains_element "lesspipe"; then
   export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
+elif hash lesspipe 2>/dev/null; then
+  # make less more friendly for non-text input files, see lesspipe(1)
+  [ -x "$(which lesspipe)" ] && eval "$(SHELL=/bin/sh lesspipe)"
 fi
