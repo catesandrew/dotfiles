143ca3fc16dca44f8b3ff1e840a2911a34a7200e
Andrew Cates <catesandrew@gmail.com>
2017-11-30 06:25:16 -0800

    fix go path in linux

--- home/.bash/plugins/go.plugins.bash
+++ home/.bash/plugins/go.plugins.bash
@@ -3,6 +3,11 @@
 # export GOROOT=$(brew --prefix go)
 if brew_contains_element "go" || \
     hash go 2>/dev/null; then
+  if [ "$__dot_system_type" == "Linux" ]; then
+    export GOROOT="${BREW_HOME}/opt/go/libexec"
+    export GOPATH="${BREW_HOME}/../go"
+  else
     export GOROOT="${BREW_HOME}/opt/go/libexec"
     export GOPATH="${BREW_HOME}/go"
+  fi
 fi
