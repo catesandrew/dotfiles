a04af9d20f37fc4ef147947b9cc8ac87659040b3
Andrew Cates <catesandrew@gmail.com>
2018-08-29 05:57:32 -0700

    update brew home location

--- home/.bash/aliases/emacs.aliases.bash
+++ home/.bash/aliases/emacs.aliases.bash
@@ -13,21 +13,21 @@ esac
 
 function __emacs_free
 {
-    # start new instances with -n -a
-    # (command open -n -a /Applications/Emacs.app "$@") &
-    (/usr/local/bin/emacs "$@" >/dev/null 2>&1) &
+  # start new instances with -n -a
+  # (command open -n -a /Applications/Emacs.app "$@") &
+  (${BREW_HOME}/bin/emacs "$@" >/dev/null 2>&1) &
 }
 
 function __emacs_client
 {
-    # version 1
-    # (exec emacsclient -c -a "" "$@") &
+  # version 1
+  # (exec emacsclient -c -a "" "$@") &
 
-    # version 2
-    # http://stackoverflow.com/questions/778716/how-can-i-make-emacs-start-up-faster
-    # (exec emacsclient -c -e "(let ((default-directory \"$(pwd)/\")) (select-frame (make-frame)) (find-file \"$1\"))" 2&>/dev/null) &
+  # version 2
+  # http://stackoverflow.com/questions/778716/how-can-i-make-emacs-start-up-faster
+  # (exec emacsclient -c -e "(let ((default-directory \"$(pwd)/\")) (select-frame (make-frame)) (find-file \"$1\"))" 2&>/dev/null) &
 
-    # version 3
-    # emacsclient -t $@ || (emacs --daemon && emacsclient -t $@)
-    (exec emacsclient -c "$@" >/dev/null 2>&1) &
+  # version 3
+  # emacsclient -t $@ || (emacs --daemon && emacsclient -t $@)
+  (exec ${BREW_HOME}/bin/emacsclient -c "$@" >/dev/null 2>&1) &
 }
