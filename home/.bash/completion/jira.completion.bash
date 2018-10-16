a90475232b34bffa46897c2bd4a79a2f51ca18bc
Andrew Cates <catesandrew@gmail.com>
2018-10-16 11:03:11 -0700

    update jira completion

--- home/.bash/completion/jira.completion.bash
+++ home/.bash/completion/jira.completion.bash
@@ -1,25 +1,5 @@
-#have wm && {
-_jira()
-{
-    local cur prev
+if brew_contains_element "go-jira" || \
+    hash jira 2>/dev/null; then
 
-    COMPREPLY=()
-    _get_comp_words_by_ref cur prev
-
-    case $prev in
-        jira)
-            COMPREPLY=( $( compgen -W 'help title html2md' -- "$cur" ) )
-            return 0
-            ;;
-    esac
-    #echo "Cur: $cur, Prev: $prev"
-    if [[ "$cur" == -* ]]; then
-        # relevant options completion
-        COMPREPLY=( $( compgen -W 'help title html2md' -- "$cur" ) )
-    fi
-
-}
-complete -F _jira jira
-#}
-
-
+  eval "$(jira --completion-script-bash)"
+fi
