4b36d0abda57ddf846db12d9ab88375a05cce17e
Andrew Cates <catesandrew@gmail.com>
2018-10-16 11:05:22 -0700

    add links to jira go templates

--- home/.bash/completion/jira.completion.bash
+++ home/.bash/completion/jira.completion.bash
@@ -1,5 +1,7 @@
 if brew_contains_element "go-jira" || \
     hash jira 2>/dev/null; then
 
+  # https://gohugo.io/templates/introduction/
+  # https://golang.org/pkg/text/template/
   eval "$(jira --completion-script-bash)"
 fi
