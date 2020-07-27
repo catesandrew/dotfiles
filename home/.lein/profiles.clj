0424f3f0cec90acbb5980362941ca154e7b44ba1
Andrew Cates <catesandrew@gmail.com>
2020-07-27 16:39:29 -0700

    add bitbucket ssh config

--- home/.lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user
- {:dependencies [[slamhound "1.5.5"]]
-  :plugins [[cider/cider-nrepl "0.9.1"]
-            [refactor-nrepl "1.0.5"]
-            [lein-cljfmt "0.1.10"]
-            [lein-try "0.4.3"]]
-  :repl-options {:timeout 300000}}}
