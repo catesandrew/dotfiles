cf6fa840b74403971885b22468337edc96be82f5
Andrew Cates <catesandrew@gmail.com>
2020-07-29 17:06:25 -0700

    fix fzf persistent history lookup

--- home/.bash/plugins/fzf.plugins.bash
+++ home/.bash/plugins/fzf.plugins.bash
@@ -7,10 +7,10 @@ if brew_contains_element "fzf" && \
   export FZF_TMUX_HEIGHT=${FZF_TMUX_HEIGHT:-80%}
 
   # Auto-completion
-  [[ $- == *i* ]] && . "${FZF_TARGET}/shell/completion.bash" 2> /dev/null
+  [[ $- == *i* ]] && source "${FZF_TARGET}/shell/completion.bash" 2> /dev/null
 
   # Key bindings
-  . "${FZF_TARGET}/shell/key-bindings.bash"
+  source "${FZF_TARGET}/shell/key-bindings.bash"
 
   # Setting ag as the default source for fzf
   export FZF_DEFAULT_COMMAND='rg --files --no-ignore --no-config --hidden --follow --glob "!.git/*"'
@@ -18,23 +18,13 @@ if brew_contains_element "fzf" && \
   export FZF_ALT_C_COMMAND="bfs -type d -nohidden | \sed s/^\./~/"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-  _fzf_complete_kill() {
-    [ -n "${COMP_WORDS[COMP_CWORD]}" ] && return 1
-    local selected fzf
-    fzf="$(__fzfcmd_complete)"
-    selected=$(command psgrep -e | \sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" $fzf -m | awk '{print $2}' | tr '\n' ' ')
-    printf '\e[5n'
-    if [ -n "$selected" ]; then
-      COMPREPLY=( "$selected" )
-      return 0
-    fi
-  }
-
+  # This changes https://github.com/junegunn/fzf/commit/5cae8ea733e8663eae96480b5e6d4c94afcdb3bd#diff-1fabf11f4aca2d62eb64290f66d25217
+  # investigate how to use fc with a different history file
   __fzf_history__() (
     local line
     shopt -u nocaseglob nocasematch
     line=$(
-      { export HISTTIMEFORMAT= && export HISTFILE=${HOME}/.persistent_history && history -cr $HISTFILE && history; } | tac | sort --key=2.1 -bus | sort -n |
+      { export HISTTIMEFORMAT= && export HISTFILE=${HOME}/.persistent_history && builtin history -cr $HISTFILE && history; } | tac | sort --key=2.1 -bus | sort -n |
       FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS --tac --sync -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m" $(__fzfcmd) |
       command grep '^ *[0-9]') &&
       if [[ $- =~ H ]]; then
@@ -44,12 +34,18 @@ if brew_contains_element "fzf" && \
       fi
   )
 
+  # CTRL-R - Paste the selected command from history into the command line
+  bind -m emacs-standard '"\C-r": " \C-e\C-u\C-y\ey\C-u`__fzf_history__`\e\C-e\er\e^"'
+
+  # CTRL-R - Paste the selected command from history into the command line
+  bind -m vi-command '"\C-r": "\C-z\C-r\C-z"'
+  bind -m vi-insert '"\C-r": "\C-z\C-r\C-z"'
+
   # view what devices a running process has open `lsof -p 5051`
   complete -o bashdefault -o default -o nospace -F _fzf_complete_kill lsof
   # Viewing memory allocation with pmap (vmmap on osx). You can view the
   # memory allocations for a particular process with pmap (vmmap):
   complete -o bashdefault -o default -o nospace -F _fzf_complete_kill vmmap
-
 
   if brew_contains_element "cheat" || \
       hash cheat 2>/dev/null; then
