2f88bd3255f21392bfad624fbfc926cfd882d73f
Andrew Cates <catesandrew@gmail.com>
2021-01-12 08:17:47 -0800

    convert email to html markdown

--- home/.mutt/bin/send-from-mutt.sh
+++ home/.mutt/bin/send-from-mutt.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-# https://git.jonathanh.co.uk/jab2870/Dotfiles/src/branch/master/bin/.bin/emails/send-from-mutt
-
-#EMAIL=$(pass Email/protonmail | grep BridgeUsername | cut -d':' -f2)
-#~/.bin/emails/MIMEmbellish | msmtp --user "$EMAIL" "$@"
-
 # Put the message, send to stdin, in a variable
 message="$(cat -)"
-config="$HOME/.config/msmtp/config.secret"
-# Look at the first argument,
-# Use it to determine the account to use
-# If not set, assume work
-# All remaining arguments should be recipient addresses which should be passed to msmtp
+
+# Look at the first argument, use it to determine the account to use, otherwise assume work.
+# All remaining arguments should be recipient addresses which should be passed to msmtp.
 case "$(echo "$1" | tr '[A-Z]' '[a-z]')" in
 	"work") account="work"; shift ;;
 	"gmail") account="gmail"; shift ;;
@@ -24,8 +17,8 @@ cleanHeaders(){
 }
 
 echo "$message" | sed '/^$/q' | grep -q -i 'markdown: true' \
-	&& echo "$message" | cleanHeaders | convertToHtmlMultipart | msmtp --file="$config" --account="$account" "$@" \
-	|| echo "$message" | cleanHeaders | msmtp --file="$config" --account="$account" "$@"
+	&& echo "$message" | cleanHeaders | convert-to-html-multipart | msmtp --account="$account" "$@" \
+	|| echo "$message" | cleanHeaders | msmtp --account="$account" "$@"
 
 # Update notmuch database
 ( sleep 2 && notmuch new ) &
