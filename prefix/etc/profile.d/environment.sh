54c4acf0b2fbc95dabfb847e059e56d616cfffbd
Andrew Cates <catesandrew@gmail.com>
2022-07-18 10:42:39 -0700

    chore: update profile environment.sh

--- prefix/etc/profile.d/environment.sh
+++ prefix/etc/profile.d/environment.sh
@@ -4,14 +4,15 @@ if [ ! -d "$BREW_HOME" ]; then
     if hash brew 2>/dev/null; then
         BREW_HOME=$(brew --prefix)
         export BREW_HOME
-        launchctl setenv BREW_HOME "$BREW_HOME"
+        # launchctl setenv BREW_HOME "$BREW_HOME"
+
     fi
 fi
 
 if [ -d "${HOME}/.bash" ]; then
     BASH_IT="${HOME}/.bash"
     export BASH_IT
-    launchctl setenv BASH_IT "$BASH_IT"
+    # launchctl setenv BASH_IT "$BASH_IT"
 fi
 
 if ! [ -d "$RBENV_HOME" ]; then
@@ -20,18 +21,27 @@ if ! [ -d "$RBENV_HOME" ]; then
     fi
 
     export RBENV_HOME
-    launchctl setenv RBENV_HOME "$RBENV_HOME"
+    # launchctl setenv RBENV_HOME "$RBENV_HOME"
 fi
 
-if [ -d "$RBENV_ROOT" ]; then
+if [ ! -d "$RBENV_ROOT" ]; then
     if [ -d "${HOME}/.rbenv" ]; then
         RBENV_ROOT="${HOME}/.rbenv"
     fi
-    if [ -d "${RBENV_ROOT}" ]; then
+
+    export RBENV_ROOT
+    # launchctl setenv RBENV_ROOT "$RBENV_ROOT"
+fi
+
+if [ -d "$RBENV_ROOT" ]; then
+    if [ -f "${RBENV_ROOT}/version" ]; then
         RBENV_VERSION=$(cat "${RBENV_ROOT}/version")
-        PATH="${RBENV_ROOT}/shims:${PATH}"
+
+        # PATH=$(path_strip "$PATH" "${RBENV_ROOT}/shims")
+        # PATH="${RBENV_ROOT}/shims:$PATH"
+
         export RBENV_VERSION
-        launchctl setenv RBENV_VERSION "$RBENV_VERSION"
+        # launchctl setenv RBENV_VERSION "$RBENV_VERSION"
     fi
 fi
 
@@ -43,24 +53,31 @@ if [ ! -d "$NVM_DIR" ]; then
     fi
 
     export NVM_DIR
-    launchctl setenv NVM_DIR "$NVM_DIR"
+    # launchctl setenv NVM_DIR "$NVM_DIR"
 fi
 
 if [ -d "$NVM_DIR" ]; then
     if [ -f "${NVM_DIR}/alias/default" ]; then
         NVM_VERSION=$(cat "${NVM_DIR}/alias/default")
-        PATH="./bin:./node_modules/.bin:${NVM_DIR}/versions/node/v${NVM_VERSION}/bin:${PATH}"
+        # ESCAPED_NVM_VERSION=$(echo "${NVM_VERSION//./$'\.'}")
+        # PATH="./bin:./node_modules/.bin:${NVM_DIR}/versions/node/v${NVM_VERSION}/bin:${PATH}"
+
+        # PATH=$(path_strip "$PATH" "\./bin")
+        # PATH=$(path_strip "$PATH" "\./node_modules/\.bin")
+        # PATH=$(path_strip "$PATH" "${NVM_DIR}/versions/node/v${NVM_VERSION}/bin")
+        #
+        # PATH="${NVM_DIR}/versions/node/v${NVM_VERSION}/bin:$PATH"
+        # PATH="./node_modules/.bin:$PATH"
+        # PATH="./bin:$PATH"
+
         NVM_BIN="${NVM_DIR}/versions/node/v${NVM_VERSION}/bin"
         NVM_PATH="${NVM_DIR}/versions/node/v${NVM_VERSION}/lib/node"
-        # NPM_CONFIG_PREFIX="${NVM_DIR}/versions/node/v${NVM_VERSION}"
         export NPM_VERSION
         export NVM_BIN
         export NVM_PATH
-        # export NPM_CONFIG_PREFIX
-        launchctl setenv NVM_VERSION "$NVM_VERSION"
-        launchctl setenv NVM_BIN "$NVM_BIN"
-        launchctl setenv NVM_PATH "$NVM_PATH"
-        # launchctl setenv NPM_CONFIG_PREFIX "$NPM_CONFIG_PREFIX"
+        # launchctl setenv NVM_VERSION "$NVM_VERSION"
+        # launchctl setenv NVM_BIN "$NVM_BIN"
+        # launchctl setenv NVM_PATH "$NVM_PATH"
     fi
 fi
 
@@ -72,12 +89,12 @@ if [ ! -d "$JENV_HOME" ]; then
     fi
 
     export JENV_HOME
-    launchctl setenv JENV_HOME "$JENV_HOME"
-
-    JENV_ROOT="${HOME}/.jenv"
-    export JENV_ROOT
-    launchctl setenv JENV_ROOT "$JENV_ROOT"
+    # launchctl setenv JENV_HOME "$JENV_HOME"
 fi
+
+JENV_ROOT="${HOME}/.jenv"
+export JENV_ROOT
+# launchctl setenv JENV_ROOT "$JENV_ROOT"
 
 if [ -d "$JENV_ROOT" ]; then
     if [ -f "${JENV_ROOT}/version" ]; then
@@ -93,19 +110,16 @@ if [ -d "$JENV_ROOT" ]; then
     fi
 fi
 
-
 if [ -d "${BREW_HOME}/go" ]; then
-    export GOROOT="${BREW_HOME}/opt/go/libexec"
-    export GOPATH="${BREW_HOME}/go"
+    GOROOT="${BREW_HOME}/opt/go/libexec"
+    GOPATH="${BREW_HOME}/go"
 
-    launchctl setenv GOROOT "$GOROOT"
-    launchctl setenv GOPATH "$GOPATH"
+    export GOROOT
+    export GOPATH
+    # launchctl setenv GOROOT "$GOROOT"
+    # launchctl setenv GOPATH "$GOPATH"
 
-    # Strip other version from PATH
-    # export PATH=$(path_strip "$PATH" "${GOPATH}/bin")
-
-    PATH="${PATH}:${GOPATH}/bin"
+    # PATH="${PATH}:${GOPATH}/bin"
 fi
 
-export PATH
-launchctl setenv PATH "$PATH"
+# launchctl setenv PATH "$PATH"
