e830f3ce003de0647ca86289fb707d47d6875b43
Andrew Cates <catesandrew@gmail.com>
2022-12-05 12:07:32 -0800

    fix: ruby/python formulaes

--- home/.bash/aliases/homebrew.aliases.bash
+++ home/.bash/aliases/homebrew.aliases.bash
@@ -119,7 +119,8 @@ bup() {
             --HEAD \
             universal-ctags/universal-ctags/universal-ctags
           ;;
-        ruby@*)
+        ruby*)
+          local latest latest_semver ruby_semver installed_semver installed_version version
           # I use rbenv to manage multiple ruby installations. However, some
           # homebrew formulae depend on ruby installed through homebrew so I
           # decided to use ruby from homebrew and link it up through rbenv.
@@ -127,25 +128,53 @@ bup() {
 
           # Find the old version of Ruby installed through homebrew
           if [ -f "${RBENV_ROOT}/version" ]; then
-            RBENV_VERSION=$(head -1 "${RBENV_ROOT}/version")
+            installed_version=$(head -1 "${RBENV_ROOT}/version")
+            # 3.1
+          fi
+          if [ -n $installed_version ]; then
+            # Uninstall the old version from rbenv
+            yes | rbenv uninstall $installed_version
+            rbenv rehash
           fi
 
-          # Uninstall the old version from rbenv
-          yes | rbenv uninstall $RBENV_VERSION
-          rbenv rehash
+          installed_semver="$(brew info --json ruby | jq -r '(.[] | .versions.stable )')"
+          # 3.1.2
+          installed_version="$(cut -d '.' -f 1,2 <<< "${installed_semver}")"
+          # 3.1
 
-          # Find the new version being installed now
-          version=$(echo "${item}" | cut -d@ -f2)
-          pkg=$(echo "${item}" | cut -d@ -f1)
-          major=$(echo "$version" | cut -d. -f1)
-          installed_version="$(brew info --json "${pkg}@${major}" | jq -r '(.[] | .versions.stable )')"
+          latest="$(brew search ruby | \grep -E '^ruby(@.*)?$' | sort -r --version-sort | head -n1)"
+          # ruby@3.0
+          latest_semver="$(brew info --json "${latest}" | jq -r '(.[] | .versions.stable )')"
+          # 3.0.4
+
+          # Now, which formula is latest? `ruby` or `ruby@3.y.z`?
+          ruby_semver="$(brew info --json ruby | jq -r '(.[] | .versions.stable )')"
+          # 3.1.2
+
+          __semver_comp $ruby_semver $latest_semver
+          case $? in
+            0)
+            # they are the same
+            ;;
+            1)
+              # `ruby` is greater semver, $1 > $2
+              latest_semver=$ruby_semver
+              latest=ruby
+              ;;
+            2)
+              # `ruby@3.y.z` is greater semver, $1 < $2
+              ;;
+          esac
+
+          version="$(cut -d '.' -f 1,2 <<< "${latest_semver}")"
+          # 3.1
 
           # Now we uninstall the old version of ruby in homebrew
           HOMEBREW_NO_AUTO_UPDATE=1 \
             HOMEBREW_NO_INSTALL_UPGRADE=1 \
             brew uninstall \
             --ignore-dependencies \
-            "${pkg}@${installed_version}"
+            "ruby@${installed_version}"
 
           # Now install the new version through the update command
           HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1 \
@@ -153,14 +182,16 @@ bup() {
             HOMEBREW_NO_AUTO_UPDATE=1 \
             brew install \
             --ignore-dependencies \
-            "${item}"
+            "$latest"
 
           ln -sfv "$(realpath $(brew --prefix "ruby@${version}"))" "$RBENV_ROOT/versions/${version}"
           rbenv global "${version}" system
           rbenv rehash
           ls -al "${RBENV_ROOT}/versions"
           ;;
-        python@*)
+        python*)
+          local latest latest_semver python_semver installed_semver installed_version version
+
           mkdir -p "${PYENV_ROOT}/versions"
           pip3 freeze >| "${TMPDIR}/requirements.txt"
 
@@ -171,42 +202,75 @@ bup() {
           yes | pyenv uninstall $PYENV_VERSION
           pyenv rehash
 
-          version=$(echo "${item}" | cut -d@ -f2)
-          pkg=$(echo "${item}" | cut -d@ -f1)
-          major=$(echo "$version" | cut -d. -f1)
-          installed_version="$(brew info --json "${pkg}@${major}" | jq -r '(.[] | .versions.stable )')"
+          installed_semver="$(brew info --json python@3 | jq -r '(.[] | .versions.stable )')"
+          # 3.10.8
+          installed_version="$(cut -d '.' -f 1,2 <<< "${installed_semver}")"
+          # 3.10
+
+          latest="$(brew search python@3 | \grep -E '^python(@.*)?$' | sort -r --version-sort | head -n1)"
+          # python@3.11
+          latest_semver="$(brew info --json "${latest}" | jq -r '(.[] | .versions.stable )')"
+          # 3.11.0
+
+          # Now, which formula is latest? `python` or `python@3.y.z`?
+          python_semver="$(brew info --json python | jq -r '(.[] | .versions.stable )')"
+          # 3.1.2
+
+          __semver_comp $python_semver $latest_semver
+          case $? in
+            0)
+            # they are the same
+            ;;
+            1)
+              # `python` is greater semver, $1 > $2
+              latest_semver=$python_semver
+              latest=python
+              ;;
+            2)
+              # `python@3.y` is greater semver, $1 < $2
+              ;;
+          esac
+
+          version="$(cut -d '.' -f 1,2 <<< "${latest_semver}")"
+          # 3.11
 
           HOMEBREW_NO_AUTO_UPDATE=1 \
             HOMEBREW_NO_INSTALL_UPGRADE=1 \
             brew uninstall \
             --ignore-dependencies \
-            "${pkg}@${installed_version}"
+            "python@${installed_version}"
 
           HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1 \
             HOMEBREW_NO_INSTALL_UPGRADE=1 \
             HOMEBREW_NO_AUTO_UPDATE=1 \
             brew install \
             --ignore-dependencies \
-            "${item}"
+            "$latest"
 
           ln -sfv "$(realpath $(brew --prefix "python@${version}"))" "$PYENV_ROOT/versions/${version}"
           ln -sfv "$(realpath $(brew --prefix "python@${version}"))/Frameworks/Python.framework/Versions/${version}/include/python${version}" "$(realpath $(brew --prefix "python@${version}"))/include"
           pyenv global "${version}" system
 
           # idle pip python wheel pydoc python-config
-          rm --force "$(realpath $(brew --prefix "python@${PYENV_VERSION}"))/bin/idle"
-          rm --force "$(realpath $(brew --prefix "python@${PYENV_VERSION}"))/bin/pip"
-          rm --force "$(realpath $(brew --prefix "python@${PYENV_VERSION}"))/bin/python"
-          rm --force "$(realpath $(brew --prefix "python@${PYENV_VERSION}"))/bin/wheel"
-          rm --force "$(realpath $(brew --prefix "python@${PYENV_VERSION}"))/bin/pydoc"
-          rm --force "$(realpath $(brew --prefix "python@${PYENV_VERSION}"))/bin/python-config"
+          rm --force "$(realpath $(brew --prefix "python@${installed_version}"))/bin/idle"
+          rm --force "$(realpath $(brew --prefix "python@${installed_version}"))/bin/pip"
+          rm --force "$(realpath $(brew --prefix "python@${installed_version}"))/bin/python"
+          rm --force "$(realpath $(brew --prefix "python@${installed_version}"))/bin/wheel"
+          rm --force "$(realpath $(brew --prefix "python@${installed_version}"))/bin/pydoc"
+          rm --force "$(realpath $(brew --prefix "python@${installed_version}"))/bin/python-config"
 
-          ln -sfv $(realpath $(brew --prefix "python@${version}"))/bin/idle3 "$(realpath $(brew --prefix "python@${version}"))/bin/idle"
-          ln -sfv $(realpath $(brew --prefix "python@${version}"))/bin/pip3 "$(realpath $(brew --prefix "python@${version}"))/bin/pip"
-          ln -sfv $(realpath $(brew --prefix "python@${version}"))/bin/python3 "$(realpath $(brew --prefix "python@${version}"))/bin/python"
-          ln -sfv $(realpath $(brew --prefix "python@${version}"))/bin/wheel3 "$(realpath $(brew --prefix "python@${version}"))/bin/wheel"
-          ln -sfv $(realpath $(brew --prefix "python@${version}"))/bin/wheel3 "$(realpath $(brew --prefix "python@${version}"))/bin/pydoc"
-          ln -sfv $(realpath $(brew --prefix "python@${version}"))/bin/wheel3 "$(realpath $(brew --prefix "python@${version}"))/bin/python-config"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/idle${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/idle"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/idle${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/idle3"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/pip${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/pip"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/pip${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/pip3"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/python${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/python"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/python${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/python3"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/wheel${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/wheel"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/wheel${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/wheel3"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/wheel${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/pydoc"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/wheel${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/pydoc3"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/wheel${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/python-config"
+          ln -sfv "$(realpath $(brew --prefix "python@${version}"))/bin/wheel${version}" "$(realpath $(brew --prefix "python@${version}"))/bin/python3-config"
           pyenv rehash
 
           ls -al "${PYENV_ROOT}/versions"
@@ -269,7 +333,7 @@ bu() {
 
   echo "Upgrading '$item'"
   case "$item" in
-    python3)
+    python)
       local latest latest_semver python_semver installed_semver installed_version version
 
       mkdir -p "${PYENV_ROOT}/versions"
